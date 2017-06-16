4e5d290b925a1fb9d74628ff8c64f9ed8ef86d60
Kartik <kartiksibal@gmail.com>
2017-06-16 09:29:40 +0530

    #1 general fixes

--- api_data.py
+++ api_data.py
@@ -2,34 +2,31 @@
 
 import json
 import sys
+from urllib.request import urlopen
 
-"""Py version check"""
-if sys.version_info[0] == 3:
-    from urllib.request import urlopen
-else: 
-	from urllib import urlopen 
+def output_cve_id(type=None, name=None, version=None):
+    """Take as input, a package name, package version.
+    Queries cve-search' dataset for any reported
+    vulnerabilities of the requested package. If
+    vulnerability exists, outputs cve-id(s).
+    """
+    if not name:
+        return None
 
-def output_cve_id(type=None, name=None, version=None): 
-	"""Take as input, a package name, package version. 
-	Queries cve-search' dataset for any reported 
-	vulnerabilities of the requested package. If 
-	vulnerability exists, outputs cve-id(s).
-	"""
-	if version:
-		url = ('https://cve.circl.lu/api/search/{}/{}').format(name,version)
-	else:
-		url = ('https://cve.circl.lu/api/search/{}').format(name)
-	
-	raw_data = urlopen(url).read()
-	data = json.loads(raw_data)
+    if version:
+        url = 'https://cve.circl.lu/api/search/{name}/{version}'
+    else:
+        url = 'https://cve.circl.lu/api/search/{name}'
 
-	if data:
-		print ('Vulnerabilties Found:\n')
-		
-		for item in data['data']:
-			print (item['id'])
-	else:
-		print ('No vulnerabilites found')
+    raw_data = urlopen(url).read()
+
+    data = json.loads(raw_data)
+
+    if data:
+        for item in data['data']:
+            return item['id']
+    else:
+        return None
 
 if __name__ == '__main__':
-	output_cve_id()
+    output_cve_id()

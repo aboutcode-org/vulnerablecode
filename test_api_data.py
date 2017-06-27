240ec002b6b052eaf46f87238d31cb5b1d4c9d11
Kartik <kartiksibal@gmail.com>
2017-06-27 22:07:47 +0530

    #2 Use dict comprehension & added test cases

--- test_api_data.py
+++ test_api_data.py
@@ -1,12 +1,31 @@
+#
+# Copyright (c) 2017 nexB Inc. and others. All rights reserved.
+# http://nexb.com and https://github.com/nexB/vulnerablecode/
+# The VulnerableCode software is licensed under the Apache License version 2.0.
+# Data generated with VulnerableCode requires an acknowledgment.
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# When you publish or redistribute any data created with VulnerableCode or any VulnerableCode
+# derivative work, you must accompany this data with the following acknowledgment:
+#
+#  Generated with VulnerableCode and provided on an "AS IS" BASIS, WITHOUT WARRANTIES
+#  OR CONDITIONS OF ANY KIND, either express or implied. No content created from
+#  VulnerableCode should be considered or used as legal advice. Consult an Attorney
+#  for any legal advice.
+#  VulnerableCode is a free software code scanning tool from nexB Inc. and others.
+#  Visit https://github.com/nexB/vulnerablecode/ for support and download.
+
 import api_data as api
+import json
 
-from mock import Mock
-import pytest
-from urllib.request import urlopen
-
-test_data = '''
-{	"data": [
-        {
+test_data = """
+[{
             "Modified": "2008-11-15T00:00:00",
             "Published": "2007-02-19T21:28:00",
             "access": {
@@ -37,13 +56,14 @@ test_data = '''
             ],
             "vulnerable_configuration_cpe_2_2": [
                 "cpe:/a:mozilla:firefox:2.0:rc3"
-            ]}]}
-'''
+            ]}]
+"""
 
-def test_output_cve_id():
-    #BUG: 
-    api.urlopen.read = Mock()
-    api.urlopen.read.return_value = test_data
-    api.output_cve_id()
 
-    assert api.output_cve_id(name="test") == "CVE-2007-1004"
+def test_extract_fields():
+    fields_names = ['id', 'cvss', 'summary']
+    data = json.loads(test_data)
+    raw_data = api.extract_fields(data=data, fields_names=fields_names)
+
+    assert raw_data == [{'cvss': 4.3, 'id': 'CVE-2007-1004',
+                        'summary': 'Mozilla Firefox might allow remote'}]

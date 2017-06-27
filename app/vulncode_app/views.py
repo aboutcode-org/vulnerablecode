3aa61aa4c0a7f1a623b3daf733c12e5f46342582
Kartik <kartiksibal@gmail.com>
2017-06-27 23:07:01 +0530

    #3 Add license & .gitignore

--- app/vulncode_app/views.py
+++ app/vulncode_app/views.py
@@ -1,8 +1,32 @@
+#
+# Copyright (c) 2017 nexB Inc. and others. All rights reserved.
+# http://nexb.com and https://github.com/nexB/vulnerablecode/
+# The VulnerableCode software is licensed under the Apache License version 2.0.
+# Data generated with VulnerableCode require an acknowledgment.
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# When you publish or redistribute any data created with VulnerableCode or any VulnerableCode
+# derivative work, you must accompany this data with the following acknowledgment:
+#
+#  Generated with VulnerableCode and provided on an "AS IS" BASIS, WITHOUT WARRANTIES
+#  OR CONDITIONS OF ANY KIND, either express or implied. No content created from
+#  VulnerableCode should be considered or used as legal advice. Consult an Attorney
+#  for any legal advice.
+#  VulnerableCode is a free software code scanning tool from nexB Inc. and others.
+#  Visit https://github.com/nexB/vulnerablecode/ for support and download.
+
 from __future__ import unicode_literals
 from django.http import HttpResponse
-import json
 from django.shortcuts import render
 import vulncode_app.api_data as api
+import json
+
 
 def package(request, name):
     """
@@ -14,6 +38,7 @@ def package(request, name):
     extracted_data = api.extract_fields(raw_data, fields_names)
 
     return HttpResponse(json.dumps(extracted_data))
+
 
 def package_version(request, name, version):
     """

886d488e517ccde2d5e80e3cf77058f27a50b14c
Kartik <kartiksibal@gmail.com>
2017-07-27 16:14:44 +0530

    Scrapes data from debian & ubuntu, dump it in the D#24
    Signed-off-by: Kartik Sibal <kartiksibal@gmail.com>
    Signed-off-by: Shrish Mishra shrish409@gmail.com

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'vulncode_app.apps.VulncodeAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [

7d96b464ad524128a3560f3543ceed3c04ece80c
Kartik <kartiksibal@gmail.com>
2017-07-31 19:14:58 +0530

    Test cases for #24 * uses debian' test data to scrape * dumps data in the test DB and verifies it
    Signed-off-by: Kartik Sibal <kartiksibal@gmail.com>
    Signed-off-by: Shrish Mishra shrish409@gmail.com

---
+++ app/vulncode_app/test_data_dump.py
@@ -0,0 +1,58 @@
+#
+# Copyright (c) 2017 nexB Inc. and others. All rights reserved.
+# http://nexb.com and https://github.com/nexB/vulnerablecode/
+# The VulnerableCode software is licensed under the Apache License version 2.0.
+# Data generated with VulnerableCode require an acknowledgment.
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# When you publish or redistribute any data created with VulnerableCode or any VulnerableCode
+# derivative work, you must accompany this data with the following acknowledgment:
+#
+#  Generated with VulnerableCode and provided on an "AS IS" BASIS, WITHOUT WARRANTIES
+#  OR CONDITIONS OF ANY KIND, either express or implied. No content created from
+#  VulnerableCode should be considered or used as legal advice. Consult an Attorney
+#  for any legal advice.
+#  VulnerableCode is a free software code scanning tool from nexB Inc. and others.
+#  Visit https://github.com/nexB/vulnerablecode/ for support and download.
+
+from django.test import TestCase
+
+from vulncode_app.models import Vulnerability
+from vulncode_app.models import VulnerabilityReference
+from vulncode_app.models import Package
+from vulncode_app.data_dump import debian_dump
+
+import json
+
+from scraper import debian
+
+
+class TestDataDump(TestCase):
+    def test_data_dump(self):
+        """
+        Scrape data from Debian' main tracker, dump it
+        in the database and verify entries.
+        """
+        with open("tests/test_data/debian.json") as f:
+            test_data = json.loads(f.read())
+
+        extract_data = debian.extract_data(test_data)
+
+        for data in extract_data:
+            vulnerability = Vulnerability(summary=data.get('description'))
+            vulnerability_reference = VulnerabilityReference(reference_id=data.get('vulnerability_id'))
+            package = Package(name=data.get('package_name'), version=data.get('fixed_version'))
+
+            vulnerability.save()
+            vulnerability_reference.save()
+            package.save()
+
+        self.assertEqual(getattr(vulnerability, 'summary'), None)
+        self.assertEqual(getattr(vulnerability_reference, 'reference_id'), "TEMP-0807341-84E914")
+        self.assertEqual(getattr(package, 'name'), "git-repair")

980f8e21ed334f2c239f149a27b089b4cd922a32
Kartik <kartiksibal@gmail.com>
2017-08-15 03:56:39 +0530

    Add serializers based on models #8 * updated views * updated urls.py
    Signed-off-by: Kartik sibal <kartiksibal@gmail.com>
    Signed-off-by: Shrish Mishra shrish409@gmail.com

--- app/vulncode_app/views.py
+++ app/vulncode_app/views.py
@@ -24,8 +24,43 @@
 import json
 
 from django.http import HttpResponse
+from drf_multiple_model.views import MultipleModelAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
+from vulncode_app.serializers import VulnerabilitySerializer
+from vulncode_app.serializers import PackageSerializer
+from vulncode_app.serializers import VulnerabilityReferenceSerializer
+from vulncode_app.models import Vulnerability
+from vulncode_app.models import Package
+from vulncode_app.models import VulnerabilityReference
 from vulncode_app import api_data
+
+
+class VulnerabilityData(APIView):
+    def get(self, request, pkg_name):
+        vulnerability_data = []
+        reference_data = []
+
+        pk = Package.objects.filter(name=pkg_name).values('id')
+
+        for index in range(len(pk)):
+            vulnerability_data.append(
+                                VulnerabilitySerializer(
+                                    Vulnerability.objects.filter(pk=pk[index]['id']),
+                                    many=True).data
+                                )
+            reference_data.append(
+                            VulnerabilityReferenceSerializer(
+                                VulnerabilityReference.objects.filter(pk=pk[index]['id']),
+                                many=True).data
+                            )
+
+        package_data = PackageSerializer(Package.objects.filter(name=pkg_name), many=True)
+        data = [*vulnerability_data, *package_data.data, *reference_data]
+
+        return Response(data)
 
 
 def package(request, name):

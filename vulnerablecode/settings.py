3a2cc62cd0864fe05d7f60db57ce84d612e46830
tdruez <thomas.druez@gmail.com>
2017-09-21 10:56:34 -0300

    Restore working static settings
    Signed-off-by: Thomas Druez <tdruez@nexb.com>
    Signed-off-by: Shrish Mishra shrish409@gmail.com

--- vulnerablecode/settings.py
+++ vulnerablecode/settings.py
@@ -133,13 +133,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
-]
+STATIC_URL = '/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/

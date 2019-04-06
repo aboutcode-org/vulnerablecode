21a8496b999bac1a8dae4ee82603a369dc420955
Ranvir Singh <ranvir.singh1114@gmail.com>
2019-04-06 15:11:52 +0530

    Add travis environment variable check in settings
    Signed-off-by: Ranvir Singh <ranvir.singh1114@gmail.com>
    Signed-off-by: Shrish Mishra shrish409@gmail.com

--- vulnerablecode/settings.py
+++ vulnerablecode/settings.py
@@ -95,6 +95,10 @@ DATABASES = {
     }
 }
 
+if 'TRAVIS' in os.environ:
+    DATABASES['default']['USER'] = 'postgres'
+    DATABASES['default']['PASSWORD'] = ''
+
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)

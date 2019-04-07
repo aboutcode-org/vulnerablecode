78b55066d60502e96b621c04c0a369fe60773cdc
Ranvir Singh <ranvir.singh1114@gmail.com>
2019-04-08 00:32:29 +0530

    Add basic admin to see the files being added
    Signed-off-by: Ranvir Singh <ranvir.singh1114@gmail.com>
    Signed-off-by: Shrish Mishra shrish409@gmail.com

--- vulnerabilities/admin.py
+++ vulnerabilities/admin.py
@@ -1,9 +1,40 @@
-from vulnerabilities.models import Vulnerability
-from vulnerabilities.models import VulnerabilityReference
-from vulnerabilities.models import Package
-from vulnerabilities.models import ImpactedPackage
-from vulnerabilities.models import ResolvedPackage
-from vulnerabilities.models import PackageReference
+from django.contrib import admin
+
+from vulnerabilities.models import (
+    ImpactedPackage,
+    Package,
+    PackageReference,
+    ResolvedPackage,
+    Vulnerability,
+    VulnerabilityReference
+)
 
 
+@admin.register(Vulnerability)
+class VulnerabilityAdmin(admin.ModelAdmin):
+    pass
 
+
+@admin.register(VulnerabilityReference)
+class VulnerabilityReferenceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ImpactedPackage)
+class ImpactedPackageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ResolvedPackage)
+class ResolvedPackageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PackageReference)
+class PackageReferenceAdmin(admin.ModelAdmin):
+    pass

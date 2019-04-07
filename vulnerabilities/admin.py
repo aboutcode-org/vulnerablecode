6511e39fbc364daae032aa8eb11e4296e4f4c98b
Ranvir Singh <ranvir.singh1114@gmail.com>
2019-04-08 00:12:06 +0530

    Add admin
    Signed-off-by: Ranvir Singh <ranvir.singh1114@gmail.com>
    Signed-off-by: Shrish Mishra shrish409@gmail.com

---
+++ vulnerabilities/admin.py
@@ -0,0 +1,9 @@
+from vulnerabilities.models import Vulnerability
+from vulnerabilities.models import VulnerabilityReference
+from vulnerabilities.models import Package
+from vulnerabilities.models import ImpactedPackage
+from vulnerabilities.models import ResolvedPackage
+from vulnerabilities.models import PackageReference
+
+
+

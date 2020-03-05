e81f5a53fe4395eb660dec8254368c5d7bc48d7c
Islam ElHakmi <eslam.elhakmey3@gmail.com>
2020-03-05 13:59:48 +0200

    fix: check for 'patched_versions' key
    Signed-off-by: Islam ElHakmi <eslam.elhakmey3@gmail.com>
    Signed-off-by: Shrish Mishra shrish409@gmail.com

--- vulnerabilities/scraper/rust.py
+++ vulnerabilities/scraper/rust.py
@@ -37,11 +37,13 @@ def import_vulnerabilities():
             'affected', {}).get('functions', {}).values())
         unaffected_version_range_list = advisory['advisory'].get(
             'unaffected_versions', [])
-        patched_version_range_list = advisory['advisory']['patched_versions']
 
-        # FIXME: Make distinction between unaffected and patched packages
-        # Check https://github.com/nexB/vulnerablecode/issues/144
-        unaffected_version_range_list += patched_version_range_list
+        if advisory['advisory'].get('patched_versions'):
+            patched_version_range_list = advisory['advisory']['patched_versions']
+            
+            # FIXME: Make distinction between unaffected and patched packages
+            # Check https://github.com/nexB/vulnerablecode/issues/144
+            unaffected_version_range_list += patched_version_range_list
 
         have_unaffected_version_range = len(unaffected_version_range_list) != 0
         have_affected_version_range = len(affected_version_range_lists) != 0

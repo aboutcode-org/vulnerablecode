2672d5efdaed5765523a05d1976672683a64a93c
Haiko Schol <hs@haikoschol.com>
2020-05-11 15:52:00 +0200

    Implement Rust importer based on GitDataSource
    Signed-off-by: Haiko Schol <hs@haikoschol.com>

--- vulnerabilities/importers/rust.py
+++ vulnerabilities/importers/rust.py
@@ -1,12 +1,112 @@
-from io import BytesIO
+# Copyright (c) 2017 nexB Inc. and others. All rights reserved.
+# http://nexb.com and https://github.com/nexB/vulnerablecode/
+# The VulnerableCode software is licensed under the Apache License version 2.0.
+# Data generated with VulnerableCode require an acknowledgment.
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# When you publish or redistribute any data created with VulnerableCode or any VulnerableCode
+# derivative work, you must accompany this data with the following acknowledgment:
+#
+#  Generated with VulnerableCode and provided on an "AS IS" BASIS, WITHOUT WARRANTIES
+#  OR CONDITIONS OF ANY KIND, either express or implied. No content created from
+#  VulnerableCode should be considered or used as legal advice. Consult an Attorney
+#  for any legal advice.
+#  VulnerableCode is a free software code scanning tool from nexB Inc. and others.
+#  Visit https://github.com/nexB/vulnerablecode/ for support and download.
+
 import json
+from io import BytesIO
+from itertools import chain
+from typing import Optional
+from typing import Set
+from typing import Tuple
+from urllib.request import urlopen
+from zipfile import ZipFile
 
 from dephell_specifier import RangeSpecifier
+from packageurl import PackageURL
 import pytoml as toml
-from urllib.request import urlopen
-import urllib.request
-from urllib.error import HTTPError
-from zipfile import ZipFile
+
+from vulnerabilities.data_source import Advisory
+from vulnerabilities.data_source import GitDataSource
+
+
+class RustDataSource(GitDataSource):
+
+    def __enter__(self):
+        super(RustDataSource, self).__enter__()
+
+        if not getattr(self, '_added_files', None):
+            self._added_files, self._updated_files = self.file_changes(
+                subdir='crates',  # TODO Consider importing the advisories for cargo, rustdoc and std as well.
+                recursive=True,
+                file_ext='toml',
+            )
+
+    @property
+    def crates_api(self):
+        if not hasattr(self, '_crates_api'):
+            setattr(self, '_crates_api', CratesAPI())
+        return self._crates_api
+
+    def added_advisories(self) -> Set[Advisory]:
+        return self._load_advisories(self._added_files)
+
+    def updated_advisories(self) -> Set[Advisory]:
+        return self._load_advisories(self._updated_files)
+
+    def _load_advisories(self, files) -> Set[Advisory]:
+        files = [f for f in files if not f.endswith('-0000.toml')]  # skip temporary files
+
+        while files:
+            batch, files = files[:self.config.batch_size], files[self.config.batch_size:]
+
+            advisories = set()
+
+            for path in batch:
+                advisory = self._load_advisory(path)
+                if advisory:
+                    advisories.add(advisory)
+            yield advisories
+
+    def _load_advisory(self, path: str) -> Optional[Advisory]:
+        with open(path) as f:
+            record = toml.load(f)
+            advisory = record.get('advisory', {})
+
+        crate_name = advisory['package']
+        reference_url = advisory.get('url', '')
+        all_versions = self.crates_api.all_versions_of_crate(crate_name)
+
+        affected_ranges = {RangeSpecifier(r) for r
+                           in chain.from_iterable(record.get('affected', {}).get('functions', {}).values())}
+
+        unaffected_ranges = {RangeSpecifier(r) for r in record.get('versions', {}).get('unaffected', [])}
+        resolved_ranges = {RangeSpecifier(r) for r in record.get('versions', {}).get('patched', [])}
+
+        unaffected, affected = categorize_versions(all_versions, unaffected_ranges, affected_ranges, resolved_ranges)
+
+        cve_id = None
+        if 'aliases' in advisory:
+            for alias in advisory['aliases']:
+                if alias.startswith('CVE-'):
+                    cve_id = alias
+                    break
+
+        return Advisory(
+            summary=advisory.get('description', ''),
+            impacted_package_urls=[PackageURL(type='cargo', name=crate_name, version=v) for v in affected],
+            resolved_package_urls=[PackageURL(type='cargo', name=crate_name, version=v) for v in unaffected],
+            reference_urls=[reference_url] if reference_url else [],
+            reference_ids=[advisory['id']],
+            cve_id=cve_id,
+        )
 
 
 RUSTSEC_DB_URL = 'https://github.com/RustSec/advisory-db/archive/master.zip'
@@ -20,80 +120,57 @@ def rust_crate_advisories(url, prefix='advisory-db-master/crates/'):
                     yield toml.load(zf.open(path))
 
 
-def all_versions_of_crate(crate):
-    info_url = "https://crates.io/api/v1/crates/{}".format(crate)
-    with urlopen(info_url) as info:
-        info = json.load(info)
-        for version_info in info['versions']:
-            yield version_info['num']
+def categorize_versions(
+        all_versions: Set[str],
+        unaffected_versions: Set[RangeSpecifier],
+        affected_versions: Set[RangeSpecifier],
+        resolved_versions: Set[RangeSpecifier],
+) -> Tuple[Set[str], Set[str]]:
+    """
+    Categorize all versions of a crate according to the given version ranges.
 
+    :return: unaffected versions, affected versions
+    """
+    unaffected, affected = set(), set()
 
-def load_advisory(advisory):
-    affected_version_range_lists = list(advisory.get(
-        'affected', {}).get('functions', {}).values())
-    unaffected_version_range_list = advisory['versions'].get(
-        'unaffected', [])
-
-    patched_version_range_list = advisory['versions']['patched']
-    # FIXME: Make distinction between unaffected and patched packages
-    # Check https://github.com/nexB/vulnerablecode/issues/144
-    unaffected_version_range_list += patched_version_range_list
-
-    have_unaffected_version_range = len(unaffected_version_range_list) != 0
-    have_affected_version_range = len(affected_version_range_lists) != 0
-
-    if not (have_affected_version_range or have_unaffected_version_range):
-        return False
-
-    vuln_id = advisory['advisory']['id']
-    crate_name = advisory['advisory']['package']
-    description = advisory['advisory']['description']
-    reference = advisory['advisory'].get('url', '')
-
-    all_versions = set(all_versions_of_crate(crate_name))
-    affected_versions = set()
-    unaffected_versions = set()
+    if not any(unaffected_versions.union(affected_versions).union(resolved_versions)):
+        return unaffected, affected
 
     for version in all_versions:
-        categorised = False
-        for affected_version_range_list in affected_version_range_lists:
-            for affected_version_range in affected_version_range_list:
-                affected_version_range = RangeSpecifier(
-                    affected_version_range)
-                if version in affected_version_range:
-                    affected_versions.add(version)
-                    categorised = True
-                    break
+        if affected_versions and all([version in av for av in affected_versions]):
+            affected.add(version)
+        elif unaffected_versions and all([version in av for av in unaffected_versions]):
+            unaffected.add(version)
+        elif resolved_versions and all([version in av for av in resolved_versions]):
+            unaffected.add(version)
 
-        if have_unaffected_version_range and not categorised:
-            for unaffected_version_range in unaffected_version_range_list:
-                if version in RangeSpecifier(unaffected_version_range):
-                    unaffected_versions.add(version)
-                    break
+    # If some versions were not classified above, one or more of the given ranges might be empty, so the remaining
+    # versions default to either affected or unaffected.
+    uncategorized_versions = all_versions - unaffected.union(affected)
+    if uncategorized_versions:
+        if not affected_versions:
+            affected.update(uncategorized_versions)
+        else:
+            unaffected.update(uncategorized_versions)
 
-    if not have_unaffected_version_range:
-        unaffected_versions = all_versions - affected_versions
-
-    elif not have_affected_version_range:
-        affected_versions = all_versions - unaffected_versions
-
-    return {
-        'package_name': crate_name,
-        'vuln_id': vuln_id,
-        'fixed_versions': unaffected_versions,
-        'affected_versions': affected_versions,
-        'advisory': reference,
-        'description': description
-    }
+    return unaffected, affected
 
 
-def import_vulnerabilities():
+class CratesAPI:
+    def __init__(self, cache=None):
+        self.versions_cache = cache or {}
 
-    vulnerability_package_dicts = []
-    for advisory in rust_crate_advisories(RUSTSEC_DB_URL):
+    def all_versions_of_crate(self, crate: str) -> Set[str]:
+        if crate in self.versions_cache:
+            return self.versions_cache[crate]
 
-        loaded_advisory = load_advisory(advisory)
-        if loaded_advisory:
-            vulnerability_package_dicts.append(loaded_advisory)
+        versions = set()
+        info_url = "https://crates.io/api/v1/crates/{}".format(crate)
 
-    return vulnerability_package_dicts
+        with urlopen(info_url) as info:
+            info = json.load(info)
+            for version_info in info['versions']:
+                versions.add(version_info['num'])
+
+        self.versions_cache[crate] = versions
+        return versions

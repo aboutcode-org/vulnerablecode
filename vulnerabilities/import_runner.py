17aff0da93dd58f2775709471e88852b85643aa0
Haiko Schol <hs@haikoschol.com>
2020-05-14 17:57:45 +0200

    Clean up ImportRunner and DataSource
    Reduce the amount of converting between PackageURL and string to the
    bare minimum. Add/improve some type annotations and reduce API surface
    area.
    
    Signed-off-by: Haiko Schol <hs@haikoschol.com>
    Signed-off-by: Shrish Mishra shrish409@gmail.com

--- vulnerabilities/import_runner.py
+++ vulnerabilities/import_runner.py
@@ -33,7 +33,7 @@ from typing import Union
 import packageurl
 
 from vulnerabilities import models
-from vulnerabilities.data_source import Advisory
+from vulnerabilities.data_source import Advisory, DataSource
 from vulnerabilities.data_source import PackageURL
 
 logger = logging.getLogger(__name__)
@@ -83,9 +83,9 @@ class ImportRunner:
         logger.debug(f'Successfully finished import for {self.importer.name}.')
 
 
-def _process_added_advisories(data_source):
+def _process_added_advisories(data_source: DataSource) -> None:
     for batch in data_source.added_advisories():
-        impacted, resolved = _collect_purls(batch)
+        impacted, resolved = _collect_package_urls(batch)
         impacted, resolved = _bulk_insert_packages(impacted, resolved)
 
         vulnerabilities = _insert_vulnerabilities_and_references(batch)
@@ -93,7 +93,7 @@ def _process_added_advisories(data_source):
         _bulk_insert_impacted_and_resolved_packages(batch, vulnerabilities, impacted, resolved)
 
 
-def _process_updated_advisories(data_source):
+def _process_updated_advisories(data_source: DataSource) -> None:
     """
     TODO: Make efficient; Current implementation needs way too many DB queries.
     """
@@ -166,24 +166,19 @@ def _get_or_create_package(p: PackageURL) -> Tuple[models.Package, bool]:
     return models.Package.objects.get_or_create(**query_kwargs)
 
 
-# FIXME
-# Accepting Set[str], Set[str] instead of Set[PackageURL] Set[PackageURL]
-# is a workaround until https://github.com/package-url/packageurl-python/issues/28 is fixed.
 def _bulk_insert_packages(
-        impacted: Set[str],
-        resolved: Set[str]
-) -> Tuple[Dict[str, models.Package], Dict[str, models.Package]]:
+        impacted: Set[PackageURL],
+        resolved: Set[PackageURL]
+) -> Tuple[Dict[PackageURL, models.Package], Dict[PackageURL, models.Package]]:
 
     packages = [_package_url_to_package(p) for p in impacted.union(resolved)]
     packages = models.Package.objects.bulk_create(packages)
 
-    # impacted = {str(p) for p in impacted}
-    # resolved = {str(p) for p in resolved}
-
     impacted_packages, resolved_packages = {}, {}
 
     for pkg in packages:
-        purl = pkg.package_url
+        # Unfortunately, PackageURLMixin.package_url returns a string, not a PackageURL
+        purl = PackageURL.from_string(pkg.package_url)
 
         if purl in impacted:
             impacted_packages[purl] = pkg
@@ -194,10 +189,10 @@ def _bulk_insert_packages(
 
 
 def _bulk_insert_impacted_and_resolved_packages(
-    batch: Sequence[Advisory],
+    batch: Set[Advisory],
     vulnerabilities: Set[models.Vulnerability],
-    impacted_packages: Dict[str, models.Package],
-    resolved_packages: Dict[str, models.Package],
+    impacted_packages: Dict[PackageURL, models.Package],
+    resolved_packages: Dict[PackageURL, models.Package],
 ) -> None:
 
     impacted_refs: List[models.ImpactedPackage] = []
@@ -207,7 +202,7 @@ def _bulk_insert_impacted_and_resolved_packages(
         vuln = _advisory_to_vulnerability(advisory, vulnerabilities)
         vulnerabilities.remove(vuln)  # minor optimization
 
-        for impacted_purl in advisory.impacted_purls:
+        for impacted_purl in advisory.impacted_package_urls:
             # TODO Figure out when/how it happens that a package is missing from the dict and fix it
             p = impacted_packages.get(impacted_purl)
             if not p:
@@ -221,7 +216,7 @@ def _bulk_insert_impacted_and_resolved_packages(
             )
             impacted_refs.append(ip)
 
-        for resolved_purl in advisory.resolved_purls:
+        for resolved_purl in advisory.resolved_package_urls:
             # TODO Figure out when/how it happens that a package is missing from the dict and fix it
             p = resolved_packages.get(resolved_purl)
             if not p:
@@ -239,7 +234,7 @@ def _bulk_insert_impacted_and_resolved_packages(
     models.ResolvedPackage.objects.bulk_create(resolved_refs)
 
 
-def _insert_vulnerabilities_and_references(batch: Sequence[Advisory]) -> Set[models.Vulnerability]:
+def _insert_vulnerabilities_and_references(batch: Set[Advisory]) -> Set[models.Vulnerability]:
     """
     TODO Consider refactoring to use bulk_create() and avoid get_or_create() when possible.
     """
@@ -284,37 +279,17 @@ def _advisory_to_vulnerability(
     raise RuntimeError(f'No Vulnerability model object found for this Advisory: {advisory.summary}')
 
 
-# FIXME
-# Returning Tuple[Set[str], Set[str]] instead of Tuple[Set[PackageURL], Set[PackageURL]]
-# is a workaround until https://github.com/package-url/packageurl-python/issues/28 is fixed.
-# When changing this, also rename the function to "_collect_package_urls".
-def _collect_purls(batch: Sequence[Advisory]) -> Tuple[Set[str], Set[str]]:
+def _collect_package_urls(batch: Set[Advisory]) -> Tuple[Set[PackageURL], Set[PackageURL]]:
     impacted, resolved = set(), set()
 
     for advisory in batch:
-        # FIXME Workaround until https://github.com/package-url/packageurl-python/issues/28 is fixed
-        s = {str(p) for p in advisory.impacted_package_urls}
-        impacted.update(s)
-
-        # FIXME Workaround until https://github.com/package-url/packageurl-python/issues/28 is fixed
-        s = {str(p) for p in advisory.resolved_package_urls}
-        resolved.update(s)
+        impacted.update(advisory.impacted_package_urls)
+        resolved.update(advisory.resolved_package_urls)
 
     return impacted, resolved
 
 
-def _package_url_to_package(purl: Union[PackageURL, str]) -> models.Package:
-    # FIXME
-    # Remove support for passing purls as strings after
-    # https://github.com/package-url/packageurl-python/issues/28 got fixed
-    if isinstance(purl, str):
-        purl = PackageURL.from_string(purl)
-
-    return models.Package(
-        name=purl.name,
-        type=purl.type,
-        version=purl.version,
-        namespace=purl.namespace,
-        qualifiers=packageurl.normalize_qualifiers(purl.qualifiers, encode=True),
-        subpath=purl.subpath,
-    )
+def _package_url_to_package(purl: PackageURL) -> models.Package:
+    p = models.Package()
+    p.set_package_url(purl)
+    return p

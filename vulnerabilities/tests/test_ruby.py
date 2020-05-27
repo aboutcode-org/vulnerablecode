d50ec6bb6e22b3853aeb63f098a93b43d9dcb009
Shivam Sandbhor <shivam.sandbhor@gmail.com>
2020-05-27 10:45:03 +0530

    Rename  to . Utilise  in RubyDataSource. Add caching in rubyAPI . Fix reference_urls in RubyDataSource
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>
    Signed-off-by: Shrish Mishra shrish409@gmail.com

--- vulnerabilities/tests/test_ruby.py
+++ vulnerabilities/tests/test_ruby.py
@@ -1,3 +1,25 @@
+# Copyright (c) 2017 nexB Inc. and others. All rights reserved.
+# http://nexb.com and https://github.com/nexB/vulnerablecode/
+# The VulnerableCode software is licensed under the Apache License version 2.0.
+# Data generated with VulnerableCode require an acknowledgment.
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# When you publish or redistribute any data created with VulnerableCode or any VulnerableCode
+# derivative work, you must accompany this data with the following acknowledgment:
+#
+#  Generated with VulnerableCode and provided on an "AS IS" BASIS, WITHOUT WARRANTIES
+#  OR CONDITIONS OF ANY KIND, either express or implied. No content created from
+#  VulnerableCode should be considered or used as legal advice. Consult an Attorney
+#  for any legal advice.
+#  VulnerableCode is a free software code scanning tool from nexB Inc. and others.
+#  Visit https://github.com/nexB/vulnerablecode/ for support and download.
+
 import os
 import pathlib
 from unittest.mock import patch
@@ -6,7 +28,7 @@ from collections import OrderedDict
 
 from packageurl import PackageURL
 
-from vulnerabilities.importers.ruby import rubyDataSource
+from vulnerabilities.importers.ruby import RubyDataSource
 from vulnerabilities.data_source import GitDataSourceConfiguration
 from vulnerabilities.data_source import Advisory
 
@@ -20,13 +42,13 @@ for filepath in pathlib.Path(TEST_DATA).glob('**/*.yml'):
     MOCK_ADDED_FILES.append(filepath.absolute())
 
 
-class rubyDataSourceTest(TestCase):
+class RubyDataSourceTest(TestCase):
 
     @classmethod
     def setUpClass(cls):
         data_source_cfg = {
             'repository_url': 'https://github.com/rubysec/ruby-advisory-db.git', }
-        cls.data_src = rubyDataSource(1, config=data_source_cfg)
+        cls.data_src = RubyDataSource(1, config=data_source_cfg)
 
     @patch('vulnerabilities.importers.ruby.rubyAPI.get_all_version_of_package',
            return_value={'1.0.0', '1.8.0', '2.0.3'})
@@ -60,7 +82,7 @@ class rubyDataSourceTest(TestCase):
                         version='2.0.3',
                         qualifiers=OrderedDict(),
                         subpath=None)},
-                reference_urls='https://github.com/sinatra/sinatra/pull/1379',
+                reference_urls=['https://github.com/sinatra/sinatra/pull/1379'],
                 reference_ids=[],
                 cve_id='CVE-2018-7212'),
             Advisory(
@@ -89,7 +111,7 @@ class rubyDataSourceTest(TestCase):
                         version='2.0.3',
                         qualifiers=OrderedDict(),
                         subpath=None)},
-                reference_urls='https://github.com/sinatra/sinatra/issues/1428',
+                reference_urls=['https://github.com/sinatra/sinatra/issues/1428'],
                 reference_ids=[],
                 cve_id='CVE-2018-11627'),
             None}

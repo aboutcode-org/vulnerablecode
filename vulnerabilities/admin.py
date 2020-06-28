c421fe1ed912572fd41cfb666a2d3f72be06e7a8
Shivam Sandbhor <shivam.sandbhor@gmail.com>
2020-06-28 13:36:17 +0530

    Refactor ImpactedPackage and ResolvedPackage into one Vulnerability_Package_Relation
    Changes are made in import_runner.py to refactore the models.
    Other major changes in import_runner.py is to change the _process_updated_advisories
    method to use less db queries and increase performance by several times.
    
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>

--- vulnerabilities/admin.py
+++ vulnerabilities/admin.py
@@ -24,10 +24,9 @@
 from django.contrib import admin
 
 from vulnerabilities.models import (
-    ImpactedPackage,
+    Vulnerability_Package_Relation,
     Importer,
     Package,
-    ResolvedPackage,
     Vulnerability,
     VulnerabilityReference,
 )
@@ -48,13 +47,8 @@ class PackageAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(ImpactedPackage)
-class ImpactedPackageAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(ResolvedPackage)
-class ResolvedPackageAdmin(admin.ModelAdmin):
+@admin.register(Vulnerability_Package_Relation)
+class Vulnerability_Package_RelationAdmin(admin.ModelAdmin):
     pass
 
 

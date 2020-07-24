b99675df3762adeca91aa4ef370f04f8a0bd5bce
Shivam Sandbhor <shivam.sandbhor@gmail.com>
2020-07-24 12:06:43 +0530

    Don't use CDN, self host instead
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>

--- vulnerablecode/urls.py
+++ vulnerablecode/urls.py
@@ -7,14 +7,14 @@
 # You may not use this software except in compliance with the License.
 # You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
 # Unless required by applicable law or agreed to in writing, software distributed
-# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# under the License is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 #
 # When you publish or redistribute any data created with VulnerableCode or any VulnerableCode
 # derivative work, you must accompany this data with the following acknowledgment:
 #
-#  Generated with VulnerableCode and provided on an "AS IS" BASIS, WITHOUT WARRANTIES
+#  Generated with VulnerableCode and provided on an 'AS IS' BASIS, WITHOUT WARRANTIES
 #  OR CONDITIONS OF ANY KIND, either express or implied. No content created from
 #  VulnerableCode should be considered or used as legal advice. Consult an Attorney
 #  for any legal advice.
@@ -48,24 +48,24 @@ api_router.register(r'packages', PackageViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^api/', include(api_router.urls)),
-    path('packages/search', PackageSearchView.as_view(), name="package_search"),
-    path('packages/<int:pk>', PackageUpdate.as_view(), name="package_view"),
+    path('packages/search', PackageSearchView.as_view(), name='package_search'),
+    path('packages/<int:pk>', PackageUpdate.as_view(), name='package_view'),
     path('vulnerabilities/<int:pk>',
-         VulnerabilityDetails.as_view(), name="vulnerability_view"),
+         VulnerabilityDetails.as_view(), name='vulnerability_view'),
     path('vulnerabilities/search', VulnerabilitySearchView.as_view(),
-         name="vulnerability_search"),
+         name='vulnerability_search'),
     path('vulnerabilities/create', VulnerabilityCreate.as_view(),
-         name="vulnerability_create"),
-    path('packages/create', PackageCreate.as_view(), name="package_create"),
+         name='vulnerability_create'),
+    path('packages/create', PackageCreate.as_view(), name='package_create'),
     path('relations/resolved/<int:pid>/<int:vid>',
-         ResolvedPackageDelete.as_view(), name="resolved_package_delete"),
+         ResolvedPackageDelete.as_view(), name='resolved_package_delete'),
     path('relations/impacted/<int:pid>/<int:vid>',
-         ImpactedPackageDelete.as_view(), name="impacted_package_delete"),
+         ImpactedPackageDelete.as_view(), name='impacted_package_delete'),
     path('relations/impacted/<int:pid>/create',
-         ImpactedPackageCreate.as_view(), name="impacted_package_create"),
+         ImpactedPackageCreate.as_view(), name='impacted_package_create'),
     path('relations/resolved/<int:pid>/create',
-         ResolvedPackageCreate.as_view(), name="resolved_package_create"),
+         ResolvedPackageCreate.as_view(), name='resolved_package_create'),
     path('relations/reference/<int:vid>/create',
-         VulnerabilityReferenceCreate.as_view(), name="vulnerability_reference_create"),
-    path('', HomePage.as_view(), name="home")
+         VulnerabilityReferenceCreate.as_view(), name='vulnerability_reference_create'),
+    path('', HomePage.as_view(), name='home')
 ]

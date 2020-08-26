f5bd3bc96ecf2ec35b6da0618ff5ea0895538d1e
Shivam Sandbhor <shivam.sandbhor@gmail.com>
2020-08-26 10:30:27 +0530

    :recycle: Refactor serailizer methods into nested serializer
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>

--- vulnerabilities/api.py
+++ vulnerabilities/api.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2017 nexB Inc. and others. All rights reserved.
+# Copyright (c) nexB Inc. and others. All rights reserved.
 # http://nexb.com and https://github.com/nexB/vulnerablecode/
 # The VulnerableCode software is licensed under the Apache License version 2.0.
 # Data generated with VulnerableCode require an acknowledgment.
@@ -18,7 +18,7 @@
 #  OR CONDITIONS OF ANY KIND, either express or implied. No content created from
 #  VulnerableCode should be considered or used as legal advice. Consult an Attorney
 #  for any legal advice.
-#  VulnerableCode is a free software code scanning tool from nexB Inc. and others.
+#  VulnerableCode is a free software tool from nexB Inc. and others.
 #  Visit https://github.com/nexB/vulnerablecode/ for support and download.
 
 from urllib.parse import unquote
@@ -44,49 +44,43 @@ class VulnerabilityReferenceSerializer(serializers.ModelSerializer):
         ]
 
 
+class HyperLinkedPackageSerializer(serializers.HyperlinkedModelSerializer):
+    purl = serializers.CharField(source="package_url")
+
+    class Meta:
+        model = Package
+        fields = ["url", "purl"]
+
+
+class HyperLinkedVulnerabilitySerializer(serializers.HyperlinkedModelSerializer):
+    vulnerability_id = serializers.CharField(source="cve_id")
+
+    class Meta:
+        model = Vulnerability
+        fields = ["url", "vulnerability_id"]
+
+
 class VulnerabilitySerializer(serializers.ModelSerializer):
     references = VulnerabilityReferenceSerializer(many=True, source="vulnerabilityreference_set")
-    resolved_packages = serializers.SerializerMethodField()
-    unresolved_packages = serializers.SerializerMethodField()
+    resolved_packages = HyperLinkedPackageSerializer(
+        many=True, source="resolved_to", read_only=True
+    )
+    unresolved_packages = HyperLinkedPackageSerializer(
+        many=True, source="vulnerable_to", read_only=True
+    )
 
     class Meta:
         model = Vulnerability
         fields = "__all__"
 
-    def get_resolved_packages(self, vulnerability):
-        request = self.context.get("request")
-
-        # Instead of iterating and querying the db for eve id FOR EACH  item
-        # in `resolved_to`, consider prefetching all cve_id of `resolved_to` in one query.
-        return [
-            {
-                "purl": Package.objects.get(id=rel.package_id).package_url,
-                "url": request.build_absolute_uri(
-                    reverse("package-detail", kwargs={"pk": rel.package_id})
-                ),
-            }
-            for rel in vulnerability.resolved_to
-        ]
-
-    def get_unresolved_packages(self, vulnerability):
-        request = self.context.get("request")
-
-        # Instead of iterating and querying the db for eve id FOR EACH  item
-        # in `vulnerable_to`, consider prefetching all cve_id of vulnerable_to` in one query.
-        return [
-            {
-                "purl": Package.objects.get(id=rel.package_id).package_url,
-                "url": request.build_absolute_uri(
-                    reverse("package-detail", kwargs={"pk": rel.package_id})
-                ),
-            }
-            for rel in vulnerability.vulnerable_to
-        ]
-
 
 class PackageSerializer(serializers.ModelSerializer):
-    unresolved_vulnerabilities = serializers.SerializerMethodField()
-    resolved_vulnerabilities = serializers.SerializerMethodField()
+    unresolved_vulnerabilities = HyperLinkedVulnerabilitySerializer(
+        many=True, source="vulnerable_to", read_only=True
+    )
+    resolved_vulnerabilities = HyperLinkedVulnerabilitySerializer(
+        many=True, source="resolved_to", read_only=True
+    )
     purl = serializers.CharField(source="package_url")
 
     class Meta:
@@ -101,36 +95,6 @@ class PackageSerializer(serializers.ModelSerializer):
             "purl",
             "resolved_vulnerabilities",
             "unresolved_vulnerabilities",
-        ]
-
-    def get_unresolved_vulnerabilities(self, package):
-        request = self.context["request"]
-
-        # Instead of iterating and querying the db for eve id FOR EACH  item
-        # in `vulnerable_to`, consider prefetching all cve_id of vulnerable_to` in one query.
-        return [
-            {
-                "vulnerability_id": Vulnerability.objects.get(id=i.vulnerability_id).cve_id,
-                "url": request.build_absolute_uri(
-                    reverse("vulnerability-detail", kwargs={"pk": i.vulnerability_id})
-                ),
-            }
-            for i in package.vulnerable_to
-        ]
-
-    def get_resolved_vulnerabilities(self, package):
-        request = self.context["request"]
-
-        # Instead of iterating and querying the db for eve id FOR EACH  item
-        # in `resolved_to`, consider prefetching all cve_id of `resolved_to` in one query.
-        return [
-            {
-                "vulnerability_id": Vulnerability.objects.get(id=i.vulnerability_id).cve_id,
-                "url": request.build_absolute_uri(
-                    reverse("vulnerability-detail", kwargs={"pk": i.vulnerability_id})
-                ),
-            }
-            for i in package.resolved_to
         ]
 
 
@@ -162,20 +126,17 @@ class PackageViewSet(viewsets.ReadOnlyModelViewSet):
     filterset_class = PackageFilterSet
 
 
+class VulnerabilityFilterSet(filters.FilterSet):
+    vulnerability_id = filters.CharFilter(field_name="cve_id")
+
+    class Meta:
+        model = Vulnerability
+        fields = ["vulnerability_id"]
+
+
 class VulnerabilityViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Vulnerability.objects.all()
     serializer_class = VulnerabilitySerializer
     paginate_by = 50
-
-    def get_queryset(self):
-        if "vulnerability_id" in self.request.query_params:
-            return Vulnerability.objects.filter(
-                cve_id__contains=self.request.query_params["vulnerability_id"]
-            )
-
-        return Vulnerability.objects.all()
-
-    def get_serializer_context(self):
-        context = super(VulnerabilityViewSet, self).get_serializer_context()
-        # Passing this context allows construction of absolute urls.
-        context.update({"request": self.request})
-        return context
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = VulnerabilityFilterSet

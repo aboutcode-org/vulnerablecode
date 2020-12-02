1160988342e9351b4099721bb691346a338c1caa
Rolf Schröder <rolf.schr@gmail.com>
2020-12-02 14:54:08 +0100

    Adapt code to actually work when flake.nix is in etc/nix.
    Signed-off-by: Rolf Schröder <rolf.schr@gmail.com>

--- etc/nix/make-poetry-conversion-patch.sh
+++ etc/nix/make-poetry-conversion-patch.sh
@@ -4,12 +4,24 @@
 # This script is used to update ./poetry-conversion.patch. Applying the patch
 # will to convert this folder into a Poetry project.
 
-GENERATED_POETRY_FILES=(pyproject.toml poetry.lock)
-PATCH_FILE=poetry-conversion.patch
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SETUP_PY=$(realpath "$THIS_DIR/../../setup.py")
+REQUIREMENTS_TXT=$(realpath "$THIS_DIR/../../requirements.txt")
+OUR_PYPROJECT_TOML="$THIS_DIR/pyproject.toml"
+OUR_POETRY_LOCK="$THIS_DIR/poetry.lock"
+GENERATED_POETRY_FILES=("$OUR_PYPROJECT_TOML" "$OUR_POETRY_LOCK")
+PATCH_FILE="$THIS_DIR/poetry-conversion.patch"
+
+# Prevent "ValueError: ZIP does not support timestamps before 1980"
+# See nixpkgs manual.
+unset SOURCE_DATE_EPOCH
 
 set -e
 
-# Sanity check.
+# Sanity checks.
+for f in $SETUP_PY $REQUIREMENTS_TXT ; do
+  test -f "$f" || { echo "File $SETUP_PY doesn't exist! Aborting ..." ; exit 1; }
+done
 for f in "${GENERATED_POETRY_FILES[@]}" ; do
   test -f "$f" && { echo "File $f exists! Aborting ..." ; exit 1; }
 done
@@ -18,7 +30,7 @@ done
 # (hopefully) all surrounding characters).
 getFromSetupPy () {
   VARIABLE_NAME=$1
-  grep -E "$VARIABLE_NAME\s?=" setup.py | sed -e 's/^.*= *//' -e 's/,.*$//' -e 's/"//g' -e "s/'//g"
+  grep -E "$VARIABLE_NAME\s?=" "$SETUP_PY" | sed -e 's/^.*= *//' -e 's/,.*$//' -e 's/"//g' -e "s/'//g"
 }
 
 NAME=$(getFromSetupPy name)
@@ -32,6 +44,9 @@ PYTHON="^3.8"
 DEFINE_MAIN_DEPS_INTERACTIVELY="no"
 DEFINE_DEV_DEPS_INTERACTIVELY="no"
 CONFIRM_GENERATION="yes"
+
+# Make sure we run from here.
+cd "$THIS_DIR"
 
 # Create the pyproject.toml file using `poetry init` which runs interactively
 # and asks a couple of questions. Answer them using predefined values.
@@ -49,12 +64,12 @@ EOF
 
 # Convert requirements.txt entries to pyproject.toml entries.
 # https://github.com/python-poetry/poetry/issues/663
-perl -pe 's/([<=>]+)/:$1/' requirements.txt | tr '\n' ' ' | xargs -t -I {} bash -c "poetry add {}"
+perl -pe 's/([<=>]+)/:$1/' "$REQUIREMENTS_TXT" | tr '\n' ' ' | xargs -t -I {} bash -c "poetry add {}"
 
 # Generate the patch file.
-rm $PATCH_FILE
+rm "$PATCH_FILE"
 for f in "${GENERATED_POETRY_FILES[@]}" ; do
-  diff -u /dev/null "$f" >> $PATCH_FILE || true # we expect differences
+  diff -u /dev/null "$f" >> "$PATCH_FILE" || true # we expect differences
 done
 
 # Remove poetry files again.

55f1d8419dcde49ab656d3f18fa02aec3c66ac5e
Shivam Sandbhor <shivam.sandbhor@gmail.com>
2021-02-19 10:35:55 +0530

    Remove debug print statement
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>

--- vulnerabilities/importers/github.py
+++ vulnerabilities/importers/github.py
@@ -253,7 +253,6 @@ class GitHubAPIDataSource(DataSource):
                                 vuln_references=vuln_references,
                             )
                         )
-                        print(adv_list[-1])
         return adv_list
 
     @staticmethod

d7d3206ba1af98410b91655bfc96a147175eca75
Amit_Gupta <amitgupta758000@gmail.com>
2021-05-08 20:53:49 +0530

    Update Apache_httpd importer from XML to JSON Advisory (#425)
    * Update Apache httpd importer from importing XML to JSON data
    
    Signed-off-by: AmitGupta7580 <amitgupta758000@gmail.com>

--- vulnerabilities/importers/apache_httpd.py
+++ vulnerabilities/importers/apache_httpd.py
@@ -20,16 +20,25 @@
 #  VulnerableCode is a free software tool from nexB Inc. and others.
 #  Visit https://github.com/nexB/vulnerablecode/ for support and download.
 
+import asyncio
 import dataclasses
-from xml.etree import ElementTree
+import urllib
 
 import requests
+from bs4 import BeautifulSoup
 from packageurl import PackageURL
+from univers.versions import MavenVersion
+from univers.version_specifier import VersionSpecifier
 
 from vulnerabilities.data_source import Advisory
 from vulnerabilities.data_source import DataSource
 from vulnerabilities.data_source import DataSourceConfiguration
+from vulnerabilities.data_source import Reference
+from vulnerabilities.data_source import VulnerabilitySeverity
+from vulnerabilities.package_managers import GitHubTagsAPI
+from vulnerabilities.severity_systems import scoring_systems
 from vulnerabilities.helpers import create_etag
+from vulnerabilities.helpers import nearest_patched_package
 
 
 @dataclasses.dataclass
@@ -40,56 +49,113 @@ class ApacheHTTPDDataSourceConfiguration(DataSourceConfiguration):
 class ApacheHTTPDDataSource(DataSource):
 
     CONFIG_CLASS = ApacheHTTPDDataSourceConfiguration
-    url = "https://httpd.apache.org/security/vulnerabilities-httpd.xml"
+    base_url = "https://httpd.apache.org/security/json/"
+
+    def set_api(self):
+        self.version_api = GitHubTagsAPI()
+        asyncio.run(self.version_api.load_api(["apache/httpd"]))
 
     def updated_advisories(self):
-        # Etags are like hashes of web responses. We maintain
-        # (url, etag) mappings in the DB. `create_etag`  creates
-        # (url, etag) pair. If a (url, etag) already exists then the code
-        # skips processing the response further to avoid duplicate work
+        links = fetch_links(self.base_url)
+        self.set_api()
+        advisories = []
+        for link in links:
+            data = requests.get(link).json()
+            advisories.append(self.to_advisory(data))
+        return self.batch_advisories(advisories)
 
-        if create_etag(data_src=self, url=self.url, etag_key="ETag"):
-            data = fetch_xml(self.url)
-            advisories = to_advisories(data)
-            return self.batch_advisories(advisories)
+    def to_advisory(self, data):
+        cve = data["CVE_data_meta"]["ID"]
+        descriptions = data["description"]["description_data"]
+        description = None
+        for desc in descriptions:
+            if desc["lang"] == "eng":
+                description = desc.get("value")
+                break
 
-        return []
-
-
-def to_advisories(data):
-    advisories = []
-    for issue in data:
-        resolved_packages = []
-        impacted_packages = []
-        for info in issue:
-            if info.tag == "cve":
-                cve = info.attrib["name"]
-
-            if info.tag == "title":
-                summary = info.text
-
-            if info.tag == "fixed":
-                resolved_packages.append(
-                    PackageURL(type="apache", name="httpd", version=info.attrib["version"])
+        severities = []
+        impacts = data.get("impact", [])
+        for impact in impacts:
+            value = impact.get("other")
+            if value:
+                severities.append(
+                    VulnerabilitySeverity(
+                        system=scoring_systems["apache_httpd"],
+                        value=value,
+                    )
                 )
-
-            if info.tag == "affects" or info.tag == "maybeaffects":
-                impacted_packages.append(
-                    PackageURL(type="apache", name="httpd", version=info.attrib["version"])
-                )
-
-        advisories.append(
-            Advisory(
-                vulnerability_id=cve,
-                summary=summary,
-                impacted_package_urls=impacted_packages,
-                resolved_package_urls=resolved_packages,
-            )
+                break
+        reference = Reference(
+            reference_id=cve,
+            url=urllib.parse.urljoin(self.base_url, f"{cve}.json"),
+            severities=severities,
         )
 
-    return advisories
+        versions_data = []
+        for vendor in data["affects"]["vendor"]["vendor_data"]:
+            for products in vendor["product"]["product_data"]:
+                for version_data in products["version"]["version_data"]:
+                    versions_data.append(version_data)
+
+        fixed_version_ranges, affected_version_ranges = self.to_version_ranges(versions_data)
+
+        affected_packages = []
+        fixed_packages = []
+
+        for version_range in fixed_version_ranges:
+            fixed_packages.extend(
+                [
+                    PackageURL(type="apache", name="httpd", version=version)
+                    for version in self.version_api.get("apache/httpd")
+                    if MavenVersion(version) in version_range
+                ]
+            )
+
+        for version_range in affected_version_ranges:
+            affected_packages.extend(
+                [
+                    PackageURL(type="apache", name="httpd", version=version)
+                    for version in self.version_api.get("apache/httpd")
+                    if MavenVersion(version) in version_range
+                ]
+            )
+
+        return Advisory(
+            vulnerability_id=cve,
+            summary=description,
+            affected_packages=nearest_patched_package(affected_packages, fixed_packages),
+            references=[reference],
+        )
+
+    def to_version_ranges(self, versions_data):
+        fixed_version_ranges = []
+        affected_version_ranges = []
+        for version_data in versions_data:
+            version_value = version_data["version_value"]
+            range_expression = version_data["version_affected"]
+            if range_expression == "<":
+                fixed_version_ranges.append(
+                    VersionSpecifier.from_scheme_version_spec_string(
+                        "maven", ">={}".format(version_value)
+                    )
+                )
+            elif range_expression == "=" or range_expression == "?=":
+                affected_version_ranges.append(
+                    VersionSpecifier.from_scheme_version_spec_string(
+                        "maven", "{}".format(version_value)
+                    )
+                )
+
+        return (fixed_version_ranges, affected_version_ranges)
 
 
-def fetch_xml(url):
-    resp = requests.get(url).content
-    return ElementTree.fromstring(resp)
+def fetch_links(url):
+    links = []
+    data = requests.get(url).content
+    soup = BeautifulSoup(data, features="lxml")
+    for tag in soup.find_all("a"):
+        link = tag.get("href")
+        if not link.endswith("json"):
+            continue
+        links.append(urllib.parse.urljoin(url, link))
+    return links

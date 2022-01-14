ae13768e83f748a836e799ef42ae92405d42119d
Hritik Vijay <hritikxx8@gmail.com>
2022-01-14 18:26:22 +0530

    Implement NginxBasicImprover
    Signed-off-by: Hritik Vijay <hritikxx8@gmail.com>
    Signed-off-by: Ziad <ziadhany2016@gmail.com>

--- vulnerabilities/importers/nginx.py
+++ vulnerabilities/importers/nginx.py
@@ -99,15 +99,15 @@ def to_advisory_data(
 
         # TODO: Mail nginx for this anomaly
         if "none" in fixed_version:
-            # FIXME: This breaks because https://github.com/nexB/univers/issues/10
+            # FIXME: Fix after https://github.com/nexB/univers/pull/31
             break
-            # affected_packages.append(
-            #     AffectedPackage(
-            #         package=PackageURL(type="generic", name="nginx", qualifiers=qualifiers),
-            #         affected_version_range=affected_version_range,
-            #     )
-            # )
-            # break
+            affected_packages.append(
+                AffectedPackage(
+                    package=PackageURL(type="generic", name="nginx", qualifiers=qualifiers),
+                    affected_version_range=affected_version_range,
+                )
+            )
+            break
 
         fixed_version = SemverVersion(fixed_version)
         purl = PackageURL(type="generic", name="nginx", qualifiers=qualifiers)
@@ -195,3 +195,79 @@ def parse_advisory_data_from_paragraph(vuln_info):
         "vulnerable": vulnerable,
         "references": references,
     }
+
+
+class NginxBasicImprover(Improver):
+    def __init__(self):
+        self.set_api()
+
+    @property
+    def interesting_advisories(self) -> QuerySet:
+        return Advisory.objects.filter(created_by=NginxDataSource.to_repr())
+
+    def get_inferences(self, advisory_data: AdvisoryData) -> Iterable[Inference]:
+        """
+        Generate and return Inferences for the given advisory data
+        """
+        try:
+            purl, affected_version_ranges, fixed_versions = AffectedPackage.merge(
+                advisory_data.affected_packages
+            )
+        except KeyError:
+            return iter([])
+        all_versions = self.version_api.get("nginx/nginx").valid_versions
+        affected_purls = []
+        for affected_version_range in affected_version_ranges:
+            for version in all_versions:
+                version = SemverVersion(version)
+                if is_vulnerable(
+                    version=version,
+                    affected_version_range=affected_version_range,
+                    fixed_versions=fixed_versions,
+                ):
+                    affected_purls.append(purl._replace(version=version))
+
+        for fixed_version in fixed_versions:
+            # TODO: This also yields with a lower fixed version, maybe we should
+            # only yield fixes that are upgrades ?
+            fixed_purl = purl._replace(version=fixed_version)
+            yield advisory_data.to_inference(
+                confidence=90,  # TODO: Decide properly
+                affected_purls=affected_purls,
+                fixed_purl=fixed_purl,
+            )
+
+    def set_api(self):
+        self.version_api = GitHubTagsAPI()
+        asyncio.run(self.version_api.load_api(["nginx/nginx"]))
+
+        # Nginx tags it's releases are in the form of `release-1.2.3`
+        # Chop off the `release-` part here.
+        normalized_versions = set()
+        while self.version_api.cache["nginx/nginx"]:
+            version = self.version_api.cache["nginx/nginx"].pop()
+            normalized_version = Version(
+                version.value.replace("release-", ""), version.release_date
+            )
+            normalized_versions.add(normalized_version)
+        self.version_api.cache["nginx/nginx"] = normalized_versions
+
+
+def is_vulnerable(version, affected_version_range, fixed_versions):
+    # Check if the version is in "Vulnerable" range.  If it's not, the
+    # version is not vulnerable.
+    #
+    # If it is, check if the branch is explicitly listed in the "Not
+    # vulnerable".  If it's not, the version is vulnerable.  If it
+    # is, check the minor number: if it's greater or equal to the
+    # version listed as not vulnerable, the version is not vulnerable,
+    # else the version is vulnerable.
+    #
+    # See: https://marc.info/?l=nginx&m=164070162912710&w=2
+
+    if version in NginxVersionRange.from_string(affected_version_range.to_string()):
+        for fixed_version in fixed_versions:
+            if version.value.minor == fixed_version.value.minor and version >= fixed_version:
+                return False
+        return True
+    return False
--- vulnerabilities/improvers/__init__.py
+++ vulnerabilities/improvers/__init__.py
@@ -1,5 +1,6 @@
 from . import default
+from .. import importers
 
-IMPROVER_REGISTRY = [default.DefaultImprover]
+IMPROVER_REGISTRY = [default.DefaultImprover, importers.nginx.NginxBasicImprover]
 
 improver_mapping = {f"{x.__module__}.{x.__name__}": x for x in IMPROVER_REGISTRY}

925e31e2f05fc2ec2578122d07df3b2c45cb39d9
Philippe Ombredanne <pombredanne@nexb.com>
2022-01-25 22:46:08 +0100

    Correct file format
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>
    Signed-off-by: Ziad <ziadhany2016@gmail.com>

--- vulnerabilities/views.py
+++ vulnerabilities/views.py
@@ -77,7 +77,7 @@ class PackageSearchView(View):
         return list(
             models.Package.objects.all()
             # FIXME: This filter is wrong and ignoring most of the fields needed for a
-            # proper package lookup: type/namespace/name@version?qualifiers and so on 
+            # proper package lookup: type/namespace/name@version?qualifiers and so on
             .filter(name__icontains=package_name, type__icontains=package_type)
             .annotate(
                 vulnerability_count=Count(

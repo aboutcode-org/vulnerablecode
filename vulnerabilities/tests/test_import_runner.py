d7bc7b41f330786e77dbf96bd0cbf5cc0e2483fb
Hritik Vijay <hritikxx8@gmail.com>
2022-02-06 02:03:33 +0530

    Add tests
    Tests for:
    	* manage.py import command
    	* manage.py improve command
    	* import_runner
    
    Signed-off-by: Hritik Vijay <hritikxx8@gmail.com>

--- vulnerabilities/tests/test_import_runner.py
+++ vulnerabilities/tests/test_import_runner.py
@@ -20,260 +20,89 @@
 #  for any legal advice.
 #  VulnerableCode is a free software code scanning tool from nexB Inc. and others.
 #  Visit https://github.com/nexB/vulnerablecode/ for support and download.
-import dataclasses
-from copy import deepcopy
 from datetime import datetime
+from datetime import timezone
+
+from univers.version_range import VersionRange
 
 from vulnerabilities import models
-from vulnerabilities.importer import Advisory
+from vulnerabilities.importer import AdvisoryData
 from vulnerabilities.importer import Importer
 from vulnerabilities.importer import PackageURL
 from vulnerabilities.importer import Reference
+from vulnerabilities.importer import AffectedPackage
 from vulnerabilities.import_runner import ImportRunner
-from vulnerabilities.helpers import AffectedPackage
+from vulnerabilities.import_runner import process_advisories
 
-# from vulnerabilities.import_runner import _insert_vulnerabilities_and_references
-
-
-class MockImporter(Importer):
-    def __init__(self, *args, **kwargs):
-        self.added_advs = kwargs.pop("added_advs", [])
-        self.updated_advs = kwargs.pop("updated_advs", [])
-        super().__init__(*args, **kwargs)
-
-    def added_advisories(self):
-        return self._yield_advisories(self.added_advs[:])
-
-    def updated_advisories(self):
-        return self._yield_advisories(self.updated_advs[:])
-
-    def _yield_advisories(self, advisories):
-        while advisories:
-            b, advisories = advisories[: self.batch_size], advisories[self.batch_size :]
-            yield b
-
-
-@dataclasses.dataclass
-class MockImporter:
-    data_source: MockImporter
-    last_run: datetime = None
-    name: str = "mock_importer"
-    license: str = "license to test"
-    saved: bool = False
-
-    def make_data_source(self, *_, **__):
-        return self.data_source
-
-    def save(self):
-        self.saved = True
-
-
-ADVISORIES = [
-    Advisory(
-        vulnerability_id="CVE-2020-13371337",
+ADVISORY_DATAS = [
+    AdvisoryData(
+        aliases=["CVE-2020-13371337"],
         summary="vulnerability description here",
-        references=[Reference(url="https://example.com/with/more/info/CVE-2020-13371337")],
         affected_packages=[
             AffectedPackage(
-                vulnerable_package=PackageURL(name="mock-webserver", type="pypi", version="1.2.33"),
-                patched_package=PackageURL(name="mock-webserver", type="pypi", version="1.2.34"),
+                package=PackageURL(type="pypi", name="dummy package"),
+                affected_version_range=VersionRange.from_string("vers:pypi/>=1.0.0|<=2.0.0"),
             )
         ],
+        references=[Reference(url="https://example.com/with/more/info/CVE-2020-13371337")],
+        date_published=datetime.now(timezone.utc),
     )
 ]
 
 
-def make_import_runner(added_advs=None, updated_advs=None):
-    added_advs = added_advs or []
-    updated_advs = updated_advs or []
+class DummyImporter(Importer):
+    spdx_license_expression = "dummy license"
 
-    importer = MockImporter(
-        data_source=MockImporter(2, added_advs=added_advs, updated_advs=updated_advs)
-    )
-
-    return ImportRunner(importer, 5)
+    def advisory_data(self):
+        return ADVISORY_DATAS
 
 
-def test_ImportRunner_new_package_and_new_vulnerability(db):
-    runner = make_import_runner(added_advs=ADVISORIES)
-
+def test_import_runner(db):
+    runner = ImportRunner(DummyImporter)
     runner.run()
-
-    assert runner.importer.last_run is not None
-    assert runner.importer.saved
-
-    assert models.Package.objects.all().count() == 2
-    packages = models.Package.objects.filter(name="mock-webserver")
-
-    if packages[0].version == "1.2.33":
-        impacted_package, resolved_package = packages[0], packages[1]
-    else:
-        impacted_package, resolved_package = packages[1], packages[0]
-
-    assert models.Vulnerability.objects.count() == 1
-    assert models.VulnerabilityReference.objects.count() == 1
-    assert models.PackageRelatedVulnerability.objects.count() == 1
-
-    assert impacted_package.vulnerable_to.count() == 1
-    assert resolved_package.resolved_to.count() == 1
-
-    vuln = impacted_package.vulnerabilities.first()
-    assert vuln.vulnerability_id == "CVE-2020-13371337"
-
-    vuln_refs = models.VulnerabilityReference.objects.filter(vulnerability=vuln)
-    assert vuln_refs.count() == 1
-    assert vuln_refs[0].url == "https://example.com/with/more/info/CVE-2020-13371337"
+    advisories = models.Advisory.objects.all()
+    advisory_datas = [x.to_advisory_data() for x in advisories]
+    assert advisory_datas == ADVISORY_DATAS
 
 
-def test_ImportRunner_existing_package_and_new_vulnerability(db):
-    """
-    Both versions of the package mentioned in the imported advisory are already in the database.
-    Only the vulnerability itself is new.
-    """
-    models.Package.objects.create(name="mock-webserver", type="pypi", version="1.2.33")
-    models.Package.objects.create(name="mock-webserver", type="pypi", version="1.2.34")
-
-    runner = make_import_runner(added_advs=ADVISORIES)
-
-    runner.run()
-
-    assert runner.importer.last_run is not None
-    assert runner.importer.saved
-
-    assert models.Vulnerability.objects.count() == 1
-    assert models.VulnerabilityReference.objects.count() == 1
-
-    assert models.PackageRelatedVulnerability.objects.count() == 1
-
-    prv = models.PackageRelatedVulnerability.objects.all()[0]
-    assert prv.patched_package.version == "1.2.34"
-
-    vuln = prv.vulnerability
-    assert vuln.vulnerability_id == "CVE-2020-13371337"
-
-    vuln_refs = models.VulnerabilityReference.objects.filter(vulnerability=vuln)
-    assert vuln_refs.count() == 1
-    assert vuln_refs[0].url == "https://example.com/with/more/info/CVE-2020-13371337"
+def test_process_advisories_with_no_advisory(db):
+    process_advisories([], "")
+    assert 0 == models.Advisory.objects.count()
 
 
-def test_ImportRunner_new_package_version_affected_by_existing_vulnerability(db):
-    """
-    Another version of a package existing in the database is added to the impacted packages of a
-    vulnerability that also already existed in the database.
-    """
-    vuln = models.Vulnerability.objects.create(
-        vulnerability_id="CVE-2020-13371337", summary="vulnerability description here"
-    )
+def test_process_advisories_with_advisories(db):
+    process_advisories(ADVISORY_DATAS, "test_importer")
+    advisories = models.Advisory.objects.all()
+    advisory_datas = [x.to_advisory_data() for x in advisories]
+    assert advisory_datas == ADVISORY_DATAS
 
-    models.VulnerabilityReference.objects.create(
-        vulnerability=vuln, url="https://example.com/with/more/info/CVE-2020-13371337"
-    )
-    models.PackageRelatedVulnerability.objects.create(
-        vulnerability=vuln,
-        package=models.Package.objects.create(name="mock-webserver", type="pypi", version="1.2.33"),
-        patched_package=models.Package.objects.create(
-            name="mock-webserver", type="pypi", version="1.2.34"
-        ),
-    )
 
-    advisories = deepcopy(ADVISORIES)
-    advisories[0].affected_packages.append(
-        AffectedPackage(
-            vulnerable_package=PackageURL(name="mock-webserver", type="pypi", version="1.2.33a")
+def test_process_advisories_idempotency(db):
+    process_advisories(ADVISORY_DATAS, "test_importer")
+    process_advisories(ADVISORY_DATAS, "test_importer")
+    process_advisories(ADVISORY_DATAS, "test_importer")
+    advisories = models.Advisory.objects.all()
+    advisory_datas = [x.to_advisory_data() for x in advisories]
+    assert advisory_datas == ADVISORY_DATAS
+
+
+def test_process_advisories_idempotency_with_one_new_advisory(db):
+    advisory_datas = ADVISORY_DATAS.copy()
+    process_advisories(advisory_datas, "test_importer")
+    advisory_datas.append(
+        AdvisoryData(
+            aliases=["CVE-2022-1337"],
         )
     )
-    runner = make_import_runner(updated_advs=advisories)
-
-    runner.run()
-
-    assert runner.importer.last_run is not None
-    assert runner.importer.saved
-
-    assert models.Package.objects.all().count() == 3
-    assert models.Vulnerability.objects.count() == 1
-    assert models.VulnerabilityReference.objects.count() == 1
-    assert models.PackageRelatedVulnerability.objects.count() == 2
-
-    qs = models.Package.objects.filter(name="mock-webserver", version="1.2.33a")
-    assert len(qs) == 1
-    added_package = qs[0]
-
-    qs = models.PackageRelatedVulnerability.objects.filter(package=added_package)
-    assert len(qs) == 1
-    impacted_package = qs[0]
-    assert impacted_package.vulnerability.vulnerability_id == "CVE-2020-13371337"
+    process_advisories(advisory_datas, "test_importer")
+    advisories = models.Advisory.objects.all()
+    advisory_datas_in_db = [x.to_advisory_data() for x in advisories]
+    assert advisory_datas_in_db == advisory_datas
 
 
-# def test_ImportRunner_fixed_package_version_is_added(db):
-#     """
-#     A new version of a package was published that fixes a previously unresolved vulnerability.
-#     """
-#     vuln = models.Vulnerability.objects.create(
-#         vulnerability_id="CVE-2020-13371337", summary="vulnerability description here"
-#     )
-
-#     models.VulnerabilityReference.objects.create(
-#         vulnerability=vuln, url="https://example.com/with/more/info/CVE-2020-13371337"
-#     )
-#     models.PackageRelatedVulnerability.objects.create(
-#         vulnerability=vuln,
-#         package=models.Package.objects.create(name="mock-webserver", type="pypi", version="1.2.33"),
-#     )
-
-#     runner = make_import_runner(updated_advs=ADVISORIES)
-
-#     runner.run()
-
-#     assert runner.importer.last_run is not None
-#     assert runner.importer.saved
-
-#     assert models.Package.objects.all().count() == 2
-#     assert models.Vulnerability.objects.count() == 1
-#     assert models.VulnerabilityReference.objects.count() == 1
-#     assert models.PackageRelatedVulnerability.objects.count() == 2
-
-#     qs = models.Package.objects.filter(name="mock-webserver", version="1.2.34")
-#     assert len(qs) == 1
-#     added_package = qs[0]
-
-#     qs = models.PackageRelatedVulnerability.objects.filter(
-#         package=added_package, is_vulnerable=False
-#     )
-#     assert len(qs) == 1
-#     resolved_package = qs[0]
-#     assert resolved_package.vulnerability.vulnerability_id == "CVE-2020-13371337"
-
-
-def test_ImportRunner_updated_vulnerability(db):
-    """
-    An existing vulnerability is updated with more information; a more detailed summary and a new
-    reference.
-    """
-    vuln = models.Vulnerability.objects.create(
-        vulnerability_id="CVE-2020-13371337", summary="temporary description"
-    )
-
-    models.PackageRelatedVulnerability.objects.create(
-        vulnerability=vuln,
-        package=models.Package.objects.create(name="mock-webserver", type="pypi", version="1.2.33"),
-        patched_package=models.Package.objects.create(
-            name="mock-webserver", type="pypi", version="1.2.34"
-        ),
-    )
-
-    runner = make_import_runner(updated_advs=ADVISORIES)
-
-    runner.run()
-
-    assert runner.importer.last_run is not None
-    assert runner.importer.saved
-
-    assert models.Package.objects.all().count() == 2
-    assert models.PackageRelatedVulnerability.objects.count() == 1
-
-    vuln = models.Vulnerability.objects.first()
-    assert vuln.summary == "vulnerability description here"
-
-    vuln_refs = models.VulnerabilityReference.objects.filter(vulnerability=vuln)
-    assert vuln_refs.count() == 1
-    assert vuln_refs[0].url == "https://example.com/with/more/info/CVE-2020-13371337"
+def test_process_advisories_idempotency_with_different_importer_names(db):
+    process_advisories(ADVISORY_DATAS, "test_importer_one")
+    process_advisories(ADVISORY_DATAS, "test_importer_two")
+    advisories = models.Advisory.objects.all()
+    advisory_datas = [x.to_advisory_data() for x in advisories]
+    assert advisory_datas == ADVISORY_DATAS

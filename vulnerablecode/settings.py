62a99bf70f1001d77ea6432a34332124a8e0e756
Philippe Ombredanne <pombredanne@nexb.com>
2022-04-07 23:41:11 +0200

    Imrpoev env var. handling in settings.py
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- vulnerablecode/settings.py
+++ vulnerablecode/settings.py
@@ -1,3 +1,5 @@
+import sys
+import tempfile
 from pathlib import Path
 
 import environ
@@ -7,33 +9,43 @@ ROOT_DIR = PROJECT_DIR.parent
 
 # Environment
 
-env_file = str(ROOT_DIR.joinpath(".env"))
-if Path(env_file).exists():
-    environ.Env.read_env(env_file)
+ENV_FILE = "/etc/vulnerablecode/.env"
+if not Path(ENV_FILE).exists():
+    ENV_FILE = ROOT_DIR(".env")
 
-env = environ.Env(TRAVIS=(bool, False))
+env = environ.Env()
+environ.Env.read_env(ENV_FILE)
+
+# Security
 
 SECRET_KEY = env.str("SECRET_KEY")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[".localhost", "127.0.0.1", "[::1]"])
-DEBUG = True
+
+# SECURITY WARNING: don't run with debug turned on in production
+DEBUG = env.bool("VULNERABLECODE_DEBUG", default=False)
+
 
 # Application definition
 
-INSTALLED_APPS = [
-    "django.contrib.admin",
+INSTALLED_APPS = (
+    # Local apps
+    # Must come before Third-party apps for proper templates override
+    "vulnerabilities",
+    # Django built-in
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "vulnerabilities",
-    "rest_framework",
+    "django.contrib.admin",
+    # Third-party apps
     "django_filters",
+    "rest_framework",
     "widget_tweaks",
-]
+)
 
-MIDDLEWARE = [
+MIDDLEWARE = (
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -41,27 +53,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
+)
 
 ROOT_URLCONF = "vulnerablecode.urls"
-
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [str(PROJECT_DIR.joinpath("templates"))],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "debug": False,
-            "context_processors": [
-                "django.contrib.auth.context_processors.auth",
-                "django.template.context_processors.debug",
-                "django.template.context_processors.static",
-                "django.template.context_processors.request",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
-]
 
 WSGI_APPLICATION = "vulnerablecode.wsgi.application"
 
@@ -71,24 +65,39 @@ WSGI_APPLICATION = "vulnerablecode.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": env.str("VULNERABLECODE_DB_ENGINE", "django.db.backends.postgresql"),
+        "HOST": env.str("VULNERABLECODE_DB_HOST", "localhost"),
         "NAME": env.str("VULNERABLECODE_DB_NAME", "vulnerablecode"),
         "USER": env.str("VULNERABLECODE_DB_USER", "vulnerablecode"),
         "PASSWORD": env.str("VULNERABLECODE_DB_PASSWORD", "vulnerablecode"),
-        "HOST": env.str("VULNERABLECODE_DB_HOST", "localhost"),
         "PORT": env.str("VULNERABLECODE_DB_PORT", "5432"),
+        "ATOMIC_REQUESTS": True,
     }
 }
 
-if env("TRAVIS"):
-    DATABASES["default"]["USER"] = "postgres"
-    DATABASES["default"]["PASSWORD"] = ""
-
-# Django 3.2 compat stuff
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
+# Templates
 
-# Password validation
-# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [str(PROJECT_DIR.joinpath("templates"))],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "debug": DEBUG,
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.request",
+                "django.template.context_processors.static",
+
+            ],
+        },
+    },
+]
+
+
+# Passwords
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -96,6 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 14,
+        },
     },
     {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
@@ -110,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = env.str("TIME_ZONE", default="UTC")
 
 USE_I18N = True
 
@@ -118,19 +130,24 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = "/static/"
+#STATIC_ROOT = "/var/vulnerablecode/static/"
 STATIC_ROOT = env.str("VULNERABLECODE_STATIC_ROOT", "./")
 
 STATICFILES_DIRS = [
     str(PROJECT_DIR.joinpath("static")),
 ]
 
-# REST API
+# Third-party apps
+
+# Django restframework
+
 REST_FRAMEWORK = {
-    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
+    "DEFAULT_FILTER_BACKENDS": (
+        "django_filters.rest_framework.DjangoFilterBackend",
+    ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 100,
 }

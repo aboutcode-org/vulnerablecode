cd15f54484cf64338b240934858d1beb27310c74
Philippe Ombredanne <pombredanne@nexb.com>
2022-04-08 19:42:52 +0200

    Enable SessionAuthentication
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- vulnerablecode/settings.py
+++ vulnerablecode/settings.py
@@ -139,10 +139,12 @@ STATICFILES_DIRS = [
 # Django restframework
 
 REST_FRAMEWORK = {
-    #"DEFAULT_AUTHENTICATION_CLASSES": (
-    #    "rest_framework.authentication.TokenAuthentication",
-    #),
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.AllowAny",
+    ),
     "DEFAULT_RENDERER_CLASSES": (
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",

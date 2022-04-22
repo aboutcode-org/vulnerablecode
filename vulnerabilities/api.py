a96bd22c28710ca27295f5749d2a01dc35e25444
Tushar Goel <tushar.goel.dav@gmail.com>
2022-04-22 23:32:54 +0530

    Allow vulnerabilities look up from a CPE
    Change models and add migrations for same, do some minor nit picks in nvd importer
    
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>
    Signed-off-by: Shrish Mishra shrish409@gmail.com

--- vulnerabilities/api.py
+++ vulnerabilities/api.py
@@ -183,26 +183,19 @@ class CPEFilterSet(filters.FilterSet):
     cpe = filters.CharFilter(method="filter_cpe")
 
     class Meta:
-        model = VulnerabilityReference
-        fields = ["reference_id"]
+        model = Vulnerability
+        fields = ["vulnerabilityreference__reference_id"]
 
     def filter_cpe(self, queryset, name, value):
         cpe = unquote(value)
-        return self.queryset.filter(reference_id=cpe)
-
-
-class CPESerializer(serializers.HyperlinkedModelSerializer):
-
-    vulnerability = MinimalVulnerabilitySerializer(read_only=True)
-
-    class Meta:
-        model = VulnerabilityReference
-        fields = ["vulnerability"]
+        return self.queryset.filter(vulnerabilityreference__reference_id__startswith=cpe).distinct()
 
 
 class CPEViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = VulnerabilityReference.objects.filter(reference_id__startswith="cpe")
-    serializer_class = CPESerializer
+    queryset = Vulnerability.objects.filter(
+        vulnerabilityreference__reference_id__startswith="cpe"
+    ).distinct()
+    serializer_class = VulnerabilitySerializer
     paginate_by = 50
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = CPEFilterSet

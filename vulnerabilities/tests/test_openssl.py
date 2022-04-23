038afc71687a888a0daa3fa3a6e3f31692641298
Keshav Priyadarshi <git@keshav.space>
2022-04-24 03:03:37 +0530

    test openssl improve and import
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>

--- vulnerabilities/tests/test_openssl.py
+++ vulnerabilities/tests/test_openssl.py
@@ -21,33 +21,49 @@
 #  Visit https://github.com/nexB/vulnerablecode/ for support and download.
 
 import datetime
+import json
 import os
 from pathlib import Path
 
 import defusedxml.ElementTree as DET
+import pytest
 from commoncode import testcase
+from django.db.models.query import QuerySet
 from packageurl import PackageURL
 from univers.version_constraint import VersionConstraint
 from univers.version_range import OpensslVersionRange
 from univers.versions import OpensslVersion
 
+from vulnerabilities import models
+from vulnerabilities.import_runner import ImportRunner
 from vulnerabilities.importer import AdvisoryData
 from vulnerabilities.importer import AffectedPackage
 from vulnerabilities.importer import Reference
-from vulnerabilities.importers.openssl import parse_vulnerabilities
-from vulnerabilities.importers.openssl import to_advisory_data
+from vulnerabilities.importers import openssl
+from vulnerabilities.improvers import default
 from vulnerabilities.tests import util_tests
+
+ADVISORY_FIELDS_TO_TEST = (
+    "unique_content_id",
+    "aliases",
+    "summary",
+    "affected_packages",
+    "references",
+    "date_published",
+)
 
 
 class TestOpenssl(testcase.FileBasedTesting):
     test_data_dir = str(Path(__file__).resolve().parent / "test_data" / "openssl")
 
     def test_parse_vulnerabilities(self):
-        xml_page = self.get_test_loc("openssl_xml_data.xml")
+        xml_page = self.get_test_loc("security_advisories.xml")
         with open(xml_page) as f:
             xml_response = f.read()
-        results = [data.to_dict() for data in parse_vulnerabilities(xml_response)]
-        expected_file = self.get_test_loc("openssl-expected.json")
+        results = [data.to_dict() for data in openssl.parse_vulnerabilities(xml_response)]
+        expected_file = self.get_test_loc(
+            "security_advisories-advisory_data-expected.json", must_exist=False
+        )
         util_tests.check_results_against_json(results, expected_file)
 
     def test_to_advisory_data(self):
@@ -111,4 +127,66 @@ class TestOpenssl(testcase.FileBasedTesting):
             date_published=datetime.datetime(2017, 12, 7, 0, 0, tzinfo=datetime.timezone.utc),
         )
         issue_parsed = DET.fromstring(issue_string)
-        assert expected == to_advisory_data(issue_parsed)
+        assert expected == openssl.to_advisory_data(issue_parsed)
+
+    @pytest.mark.django_db(transaction=True)
+    def test_OpensslImporter(self):
+
+        expected_file = self.get_test_loc(
+            "security_advisories-importer-expected.json", must_exist=False
+        )
+
+        results = self.run_import()
+        datetime_to_isoformat(results)
+        util_tests.check_results_against_json(results, expected_file)
+
+        # run again as there should be no duplicates
+        results = self.run_import()
+        datetime_to_isoformat(results)
+        util_tests.check_results_against_json(results, expected_file)
+
+    def run_import(self):
+        """
+        Return a list of imported Advisory model objects.
+        """
+
+        class MockOpensslImporter(openssl.OpensslImporter):
+            """
+            A mocked OpensslImporter that loads content from a file rather than
+            making a network call.
+            """
+
+            def fetch(self):
+                with open(test_file) as tf:
+                    return tf.read()
+
+        test_file = self.get_test_loc("security_advisories.xml")
+
+        ImportRunner(MockOpensslImporter).run()
+        return list(models.Advisory.objects.all().values(*ADVISORY_FIELDS_TO_TEST))
+
+    @pytest.mark.django_db(transaction=True)
+    def test_DefaultImprover_inferences_on_Openssl(self):
+
+        with open(self.get_test_loc("improver/improver-advisories.json")) as vf:
+            advisories_data = json.load(vf)
+
+        results = []
+        improver = default.DefaultImprover()
+        for advdata in advisories_data:
+            advisory_data = AdvisoryData.from_dict(advdata)
+
+            inferences = improver.get_inferences(advisory_data=advisory_data)
+            for i in inferences:
+                i.vulnerability_id = "PLAIN-ID-FOR-TESTING"
+                results.append(i.to_dict())
+
+        expected_file = self.get_test_loc(
+            "improver/improver-inferences-expected.json", must_exist=False
+        )
+        util_tests.check_results_against_json(results, expected_file)
+
+
+def datetime_to_isoformat(advisories):
+    for advisory in advisories:
+        advisory["date_published"] = advisory["date_published"].isoformat()

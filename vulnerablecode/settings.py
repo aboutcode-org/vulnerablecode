6d419cc041dee003b92c85d7586a149a7541bb4c
Philippe Ombredanne <pombredanne@nexb.com>
2022-10-19 16:48:55 +0200

    Enable selectively the debug toolbar
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>Signed-off-by: Shrish Mishra shrish409@gmail.com

--- vulnerablecode/settings.py
+++ vulnerablecode/settings.py
@@ -34,6 +34,9 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[".localhost", "127.0.0.1", "[
 DEBUG = env.bool("VULNERABLECODE_DEBUG", default=False)
 
 # SECURITY WARNING: don't run with debug turned on in production
+DEBUG_TOOLBAR = env.bool("VULNERABLECODE_DEBUG_TOOLBAR", default=False)
+
+# SECURITY WARNING: don't run with debug turned on in production
 DEBUG_UI = env.bool("VULNERABLECODE_DEBUG_UI", default=False)
 
 # Application definition
@@ -56,6 +59,7 @@ INSTALLED_APPS = (
     "rest_framework.authtoken",
     "widget_tweaks",
 )
+
 
 MIDDLEWARE = (
     "django.middleware.security.SecurityMiddleware",
@@ -187,3 +191,30 @@ REST_FRAMEWORK = {
 
 if not VULNERABLECODEIO_REQUIRE_AUTHENTICATION:
     REST_FRAMEWORK["DEFAULT_PERMISSION_CLASSES"] = ("rest_framework.permissions.AllowAny",)
+
+
+if DEBUG_TOOLBAR:
+    INSTALLED_APPS += ("debug_toolbar",)
+
+    MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
+
+    DEBUG_TOOLBAR_PANELS = (
+        "debug_toolbar.panels.history.HistoryPanel",
+        "debug_toolbar.panels.versions.VersionsPanel",
+        "debug_toolbar.panels.timer.TimerPanel",
+        "debug_toolbar.panels.settings.SettingsPanel",
+        "debug_toolbar.panels.headers.HeadersPanel",
+        "debug_toolbar.panels.request.RequestPanel",
+        "debug_toolbar.panels.sql.SQLPanel",
+        "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+        "debug_toolbar.panels.templates.TemplatesPanel",
+        "debug_toolbar.panels.cache.CachePanel",
+        "debug_toolbar.panels.signals.SignalsPanel",
+        "debug_toolbar.panels.logging.LoggingPanel",
+        "debug_toolbar.panels.redirects.RedirectsPanel",
+        "debug_toolbar.panels.profiling.ProfilingPanel",
+    )
+
+    INTERNAL_IPS = [
+        "127.0.0.1",
+    ]

a74e941acf7df211c901fde9ffee952a5484ce51
Philippe Ombredanne <pombredanne@nexb.com>
2022-10-20 15:20:32 +0200

    Remove unsued import
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- vulnerablecode/urls.py
+++ vulnerablecode/urls.py
@@ -7,7 +7,6 @@
 # See https://aboutcode.org for more information about nexB OSS projects.
 #
 
-from django.contrib import admin
 from django.urls import include
 from django.urls import path
 from django.urls import re_path

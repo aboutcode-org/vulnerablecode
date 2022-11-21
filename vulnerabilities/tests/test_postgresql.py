9dacb3c0b9a2d168e499868a3ac4e8face230df4
Tushar Goel <tushar.goel.dav@gmail.com>
2022-11-21 20:57:52 +0530

    Fix failing tests
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>
    Signed-off-by: Shrish Mishra shrish409@gmail.com

--- vulnerabilities/tests/test_postgresql.py
+++ vulnerabilities/tests/test_postgresql.py
@@ -7,23 +7,13 @@
 # See https://aboutcode.org for more information about nexB OSS projects.
 #
 
-import json
 import os
 
-from django.test import TestCase
-from packageurl import PackageURL
+import pytest
 
-from vulnerabilities import severity_systems
-from vulnerabilities.import_runner import process_advisories
-from vulnerabilities.importer import AdvisoryData
-from vulnerabilities.importer import Reference
-from vulnerabilities.importer import VulnerabilitySeverity
 from vulnerabilities.importers.postgresql import to_advisories
-from vulnerabilities.improve_runner import ImproveRunner
-from vulnerabilities.improve_runner import process_inferences
-from vulnerabilities.improvers.default import DefaultImprover
+from vulnerabilities.models import Package
 from vulnerabilities.tests import util_tests
-from vulnerabilities.utils import AffectedPackage
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEST_DATA = os.path.join(
@@ -32,18 +22,19 @@ TEST_DATA = os.path.join(
 )
 
 
-class TestPostgreSQLImporter(TestCase):
-    def test_to_advisories(self):
-        with open(os.path.join(TEST_DATA, "advisories.html")) as f:
-            raw_data = f.read()
-        advisories = to_advisories(raw_data)
-        result = [data.to_dict() for data in advisories]
-        expected_file = os.path.join(TEST_DATA, f"parse-advisory-postgresql-expected.json")
-        util_tests.check_results_against_json(result, expected_file)
+def test_to_advisories():
+    with open(os.path.join(TEST_DATA, "advisories.html")) as f:
+        raw_data = f.read()
+    advisories = to_advisories(raw_data)
+    result = [data.to_dict() for data in advisories]
+    expected_file = os.path.join(TEST_DATA, f"parse-advisory-postgresql-expected.json")
+    util_tests.check_results_against_json(result, expected_file)
 
-    def test_run_default_improver(self):
-        with open(os.path.join(TEST_DATA, "improver-data.json")) as f:
-            raw_data = json.load(f)
-        advisories = [AdvisoryData.from_dict(data) for data in raw_data]
-        process_advisories(advisories, "postgresql")
-        ImproveRunner(DefaultImprover).run()
+
+@pytest.mark.django_db
+def test_get_or_create_from_purl():
+    p1 = "pkg:generic/postgres"
+    p2 = "pkg:generic/postgres?foo=bar"
+    res1 = Package.objects.get_or_create_from_purl(p1)
+    res2 = Package.objects.get_or_create_from_purl(p2)
+    res3 = Package.objects.get_or_create_from_purl(p1)

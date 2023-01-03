42561b867dd2578144c953244babff10b359a82e
John M. Horan <johnmhoran@gmail.com>
2023-01-02 16:41:53 -0800

    Begin apache_tomcat.py refactoring #970
    Reference: https://github.com/nexB/vulnerablecode/issues/970
    
    Signed-off-by: John M. Horan <johnmhoran@gmail.com>

--- vulnerabilities/tests/test_apache_tomcat.py
+++ vulnerabilities/tests/test_apache_tomcat.py
@@ -19,7 +19,8 @@ from univers.versions import MavenVersion
 from vulnerabilities.importer import AdvisoryData
 from vulnerabilities.importer import Reference
 from vulnerabilities.importers.apache_tomcat import ApacheTomcatImporter
-from vulnerabilities.importers.apache_tomcat import extract_advisories_from_page
+from vulnerabilities.importers.apache_tomcat import extract_tomcat_advisory_data_from_page
+from vulnerabilities.importers.apache_tomcat import to_version_ranges
 from vulnerabilities.package_managers import MavenVersionAPI
 from vulnerabilities.package_managers import PackageVersion
 from vulnerabilities.utils import AffectedPackage
@@ -28,6 +29,13 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEST_DATA = os.path.join(BASE_DIR, "test_data/apache_tomcat")
 
 security_updates_home = "https://tomcat.apache.org/security"
+
+
+# Temp test to flesh out `extract_advisories_from_page()` -- the method .
+def test_method_extract_advisories_from_page():
+    with open(os.path.join(TEST_DATA, "apache_tomcat_cve-2020-9484.html")) as f:
+        raw_data = f.read()
+    extracted_advisories = ApacheTomcatImporter().extract_advisories_from_page(raw_data)
 
 
 def test_extract_advisories_from_page():
@@ -72,7 +80,7 @@ def test_extract_advisories_from_page():
             "fixed_versions": ["10.0.5"],
         },
     ]
-    results = extract_advisories_from_page(page)
+    results = extract_tomcat_advisory_data_from_page(page)
     results = [d.to_dict() for d in results]
     assert results == expected
 
@@ -290,14 +298,15 @@ def test_extract_advisories_from_page_with_multiple_groups():
         },
     ]
 
-    results = extract_advisories_from_page(page)
+    results = extract_tomcat_advisory_data_from_page(page)
     results = [d.to_dict() for d in results]
     assert results == expected
 
 
 # This test is temporary -- just for running apache_tomcat.py using all HTML report pages.
 # Will replace with a REGEN-based test as with apache_httpd and postgresql.
-def test_updated_advisories():
+# Formely named `test_updated_advisories` before method was renamed `advisory_data()`
+def test_advisory_data():
     returned_advisories = ApacheTomcatImporter().advisory_data()
 
 
@@ -345,9 +354,13 @@ def test_to_version_ranges_test():
         )
     )
 
-    converted_versions_data = ApacheTomcatImporter().to_version_ranges(
-        versions_data, fixed_versions
-    )
+    # XXX: 2023-01-02 Monday 16:27:37.  Error: AttributeError: 'ApacheTomcatImporter' object has no attribute 'to_version_ranges'
+    # That's because I converted a method function to an independent function.
+    # converted_versions_data = ApacheTomcatImporter().to_version_ranges(
+    #     versions_data, fixed_versions
+    # )
+    # Try this instead.  It works!
+    converted_versions_data = to_version_ranges(versions_data, fixed_versions)
 
     # print("\nvers_test = {}\n".format(MavenVersionRange.from_string("vers:maven/>=1.0.0|<=2.0.0")))
     # print("\nconverted_versions_data = {}\n".format(converted_versions_data))

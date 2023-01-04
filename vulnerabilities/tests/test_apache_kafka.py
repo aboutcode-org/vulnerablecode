7d527338f0fdadbf70b1bc47635dec0fade49cc5
John M. Horan <johnmhoran@gmail.com>
2023-01-03 16:29:58 -0800

    Remove unneeded comments and test files, add changelog entry for apache_kafka #972
    Reference: https://github.com/nexB/vulnerablecode/issues/972
    
    Signed-off-by: John M. Horan <johnmhoran@gmail.com>
    Signed-off-by: Shrish Mishra shrish409@gmail.com

--- vulnerabilities/tests/test_apache_kafka.py
+++ vulnerabilities/tests/test_apache_kafka.py
@@ -7,19 +7,15 @@
 # See https://aboutcode.org for more information about nexB OSS projects.
 #
 
-# temporarily import json to create output to analyze
-import json
 import os
 
 import pytest
 
 from vulnerabilities.importers.apache_kafka import ApacheKafkaImporter
-
-# from vulnerabilities.package_managers import GitHubTagsAPI
 from vulnerabilities.tests import util_tests
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# Created cve-list-2022-12-06.html with a full copy of https://raw.githubusercontent.com/apache/kafka-site/asf-site/cve-list.html
+
 TEST_DATA = os.path.join(
     BASE_DIR,
     "test_data/apache_kafka",
@@ -32,23 +28,10 @@ def test_to_advisory():
     advisories = ApacheKafkaImporter().to_advisory(raw_data)
     result = [data.to_dict() for data in advisories]
 
-    # TODO: We need to finish this test including the REGEN step.  2022-12-12 Monday 14:48:05.  Done.
     expected_file = os.path.join(TEST_DATA, f"to-advisory-apache_kafka-expected.json")
     util_tests.check_results_against_json(result, expected_file)
 
-    # We generate these 2 files solely to vet the output and adjust the importer code.
-    # with open(os.path.join(TEST_DATA, "jmh-test-01.txt"), "w") as f1:
-    #     for advisory_object in result:
-    #         f1.write(f"{advisory_object}\n\n")
-    #         for k, v in advisory_object.items():
-    #             f1.write(f"{k}: {v}\n\n")
-    #         f1.write(f"=================================================\n\n")
 
-    # with open(os.path.join(TEST_DATA, "test-advisories.json"), "w", encoding="utf-8") as f:
-    #     json.dump(result, f, ensure_ascii=False, indent=4)
-
-
-# Check for an unknown CVE value.
 def to_advisory_changed_cve():
     with open(os.path.join(TEST_DATA, "cve-list-changed-cve.html")) as f:
         raw_data = f.read()
@@ -62,7 +45,6 @@ def test_to_advisory_changed_cve_exception():
     assert "CVE-2022-34918" in str(excinfo.value)
 
 
-# Check for an unknown "Versions affected" value.
 def to_advisory_changed_versions_affected():
     with open(os.path.join(TEST_DATA, "cve-list-changed-versions-affected.html")) as f:
         raw_data = f.read()
@@ -76,7 +58,6 @@ def test_to_advisory_changed_versions_affected_exception():
     assert "2.8.0 - 2.8.1, 3.0.0 - 3.0.1, 3.1.0 - 3.1.1, 3.2.0 - 3.2.2" in str(excinfo.value)
 
 
-# Check for an unknown "Fixed versions" value.
 def to_advisory_changed_fixed_versions():
     with open(os.path.join(TEST_DATA, "cve-list-changed-fixed-versions.html")) as f:
         raw_data = f.read()

dcabc53d2845429d8e12b0f4c2c4a9ed2ccc8c2e
Tushar Goel <tushar.goel.dav@gmail.com>
2023-01-12 17:18:56 +0530

    Migrate suse score importer
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>

--- vulnerabilities/importers/suse_scores.py
+++ vulnerabilities/importers/suse_scores.py
@@ -7,31 +7,34 @@
 # See https://aboutcode.org for more information about nexB OSS projects.
 #
 
+from typing import Iterable
+
 from vulnerabilities import severity_systems
 from vulnerabilities.importer import AdvisoryData
 from vulnerabilities.importer import Importer
 from vulnerabilities.importer import Reference
 from vulnerabilities.importer import VulnerabilitySeverity
 from vulnerabilities.utils import fetch_yaml
+from vulnerabilities.utils import is_cve
 
 URL = "https://ftp.suse.com/pub/projects/security/yaml/suse-cvss-scores.yaml"
 
 
 class SUSESeverityScoreImporter(Importer):
-    def updated_advisories(self):
-        advisories = []
-        score_data = fetch_yaml(URL)
-        advisories.append(self.to_advisory(score_data))
-        return advisories
 
-    @staticmethod
-    def to_advisory(score_data):
+    spdx_license_expression = "CC-BY-4.0"
+    license_url = "https://ftp.suse.com/pub/projects/security/yaml/LICENSE"
+
+    def advisory_data(self) -> Iterable[AdvisoryData]:
+        score_data = fetch_yaml(URL)
+        yield from self.to_advisory(score_data)
+
+    def to_advisory(self, score_data):
         systems_by_version = {
             "2.0": severity_systems.CVSSV2,
             "3": severity_systems.CVSSV3,
             "3.1": severity_systems.CVSSV31,
         }
-        advisories = []
 
         for cve_id in score_data:
             severities = []
@@ -47,11 +50,11 @@ class SUSESeverityScoreImporter(Importer):
                 )
                 severities.append(score)
 
-            advisories.append(
-                AdvisoryData(
-                    vulnerability_id=cve_id,
-                    summary="",
-                    references=[Reference(url=URL, severities=severities)],
-                )
+            if not is_cve(cve_id):
+                continue
+
+            yield AdvisoryData(
+                aliases=[cve_id],
+                summary="",
+                references=[Reference(url=URL, severities=severities)],
             )
-        return advisories

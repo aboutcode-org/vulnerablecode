ea997e2bb6da45126bb9ae0d299556d87ee6203c
John M. Horan <johnmhoran@gmail.com>
2023-02-12 15:13:02 -0800

    Filter for name-affected.xml files, check CVE prefixes #1079
    Reference: https://github.com/nexB/vulnerablecode/issues/1079
    
    Signed-off-by: John M. Horan <johnmhoran@gmail.com>

--- vulnerabilities/importers/suse_oval.py
+++ vulnerabilities/importers/suse_oval.py
@@ -18,7 +18,6 @@ from vulnerabilities.importer import OvalImporter
 
 
 class SuseOvalImporter(OvalImporter):
-
     spdx_license_expression = "CC-BY-4.0"
     license_url = "https://ftp.suse.com/pub/projects/security/oval/LICENSE"
     base_url = "https://ftp.suse.com/pub/projects/security/oval/"
@@ -37,7 +36,7 @@ class SuseOvalImporter(OvalImporter):
             if node.get("href").endswith(".gz")
         ]
 
-        for suse_file in suse_oval_files:
+        for suse_file in filter(suse_oval_files):
             response = requests.get(suse_file)
 
             extracted = gzip.decompress(response.content)
@@ -45,3 +44,26 @@ class SuseOvalImporter(OvalImporter):
                 {"type": "rpm", "namespace": "opensuse"},
                 ET.ElementTree(ET.fromstring(extracted.decode("utf-8"))),
             )
+
+
+def filter(suse_oval_files):
+    """
+    Filter to exclude "name.xml" when we also have "name-affected.xml", e.g.,
+    "opensuse.leap.15.3.xml.gz" vs. "opensuse.leap.15.3-affected.xml.gz".  See
+    https://ftp.suse.com/pub/projects/security/oval/README: "name-affected.xml" includes
+    "fixed security issues and the analyzed issues both affecting and NOT affecting SUSE" and
+    "name.xml" includes "fixed security issues and the analyzed issues NOT affecting SUSE."
+    """
+    affected_files = [
+        affected_file for affected_file in suse_oval_files if "-affected" in affected_file
+    ]
+
+    trimmed_affected_files = [
+        affected_file.replace("-affected", "") for affected_file in affected_files
+    ]
+
+    filtered_suse_oval_files = [
+        gz_file for gz_file in suse_oval_files if gz_file not in trimmed_affected_files
+    ]
+
+    return filtered_suse_oval_files

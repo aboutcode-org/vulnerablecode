e79735f257fefb00c0ba628f9b862a689bb80417
Tushar Goel <tushar.goel.dav@gmail.com>
2023-02-28 21:02:21 +0530

    Add threads to run importers parallerly
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>

--- vulnerabilities/management/commands/import.py
+++ vulnerabilities/management/commands/import.py
@@ -7,6 +7,7 @@
 # See https://aboutcode.org for more information about nexB OSS projects.
 #
 import traceback
+from multiprocessing.dummy import Pool as ThreadPool
 
 from django.core.management.base import BaseCommand
 from django.core.management.base import CommandError
@@ -28,52 +29,58 @@ class Command(BaseCommand):
 
         parser.add_argument("sources", nargs="*", help="Fully qualified importer name to run")
 
+        parser.add_argument("--threads", type=int, default=4, help="Number of threads to use")
+
     def handle(self, *args, **options):
         if options["list"]:
             self.list_sources()
             return
 
+        threads = options["threads"]
         if options["all"]:
-            self.import_data(importers=IMPORTERS_REGISTRY.values())
+            self.import_data(importers=IMPORTERS_REGISTRY.values(), threads=threads)
             return
 
         sources = options["sources"]
         if not sources:
             raise CommandError('Please provide at least one importer to run or use "--all".')
 
-        self.import_data(validate_importers(sources))
+        self.import_data(validate_importers(sources), threads)
 
     def list_sources(self):
         self.stdout.write("Vulnerability data can be imported from the following importers:")
         self.stdout.write("\n".join(IMPORTERS_REGISTRY))
 
-    def import_data(self, importers):
+    def import_data(self, importers, threads):
         """
         Run the given ``importers``. The ``importers`` are expected to be class
         names for the importers.
         """
         failed_importers = []
-
-        for importer in importers:
-            self.stdout.write(f"Importing data using {importer.qualified_name}")
-            try:
-                ImportRunner(importer).run()
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Successfully imported data using {importer.qualified_name}"
-                    )
-                )
-            except Exception:
-                failed_importers.append(importer.qualified_name)
-                traceback.print_exc()
-                self.stdout.write(
-                    self.style.ERROR(
-                        f"Failed to run importer {importer.qualified_name}. Continuing..."
-                    )
-                )
+        pool = ThreadPool(threads)
+        results = pool.imap_unordered(self.run_importer, importers)
+        for result in results:
+            if result:
+                failed_importers.append(result.qualified_name)
 
         if failed_importers:
             raise CommandError(f"{len(failed_importers)} failed!: {','.join(failed_importers)}")
+
+    def run_importer(self, importer):
+        failed_importer = None
+        self.stdout.write(f"Importing data using {importer.qualified_name}")
+        try:
+            ImportRunner(importer).run()
+            self.stdout.write(
+                self.style.SUCCESS(f"Successfully imported data using {importer.qualified_name}")
+            )
+        except Exception:
+            failed_importer = importer
+            traceback.print_exc()
+            self.stdout.write(
+                self.style.ERROR(f"Failed to run importer {importer.qualified_name}. Continuing...")
+            )
+        return failed_importer
 
 
 def validate_importers(sources):

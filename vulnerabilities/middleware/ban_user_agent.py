4535c114281405d932aa9858886af599cf3f0d8e
Tushar Goel <34160672+TG1999@users.noreply.github.com>
2023-11-27 12:20:09 +0530

    Add middleware to ban bytedance user agent (#1347)
    * Add middleware to ban bytedance user agent
    
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>
    
    * Change response type
    
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>
    
    ---------
    
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>Signed-off-by: Shrish Mishra shrish409@gmail.com

---
+++ vulnerabilities/middleware/ban_user_agent.py
@@ -0,0 +1,18 @@
+#
+# Copyright (c) nexB Inc. and others. All rights reserved.
+# VulnerableCode is a trademark of nexB Inc.
+# SPDX-License-Identifier: Apache-2.0
+# See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
+# See https://github.com/nexB/vulnerablecode for support or download.
+# See https://aboutcode.org for more information about nexB OSS projects.
+#
+
+from django.http import HttpResponseNotFound
+from django.utils.deprecation import MiddlewareMixin
+
+
+class BanUserAgent(MiddlewareMixin):
+    def process_request(self, request):
+        user_agent = request.META.get("HTTP_USER_AGENT", None)
+        if user_agent and "bytedance" in user_agent:
+            return HttpResponseNotFound(404)

fca8815876d9d6f8bd5eab8ee9bdf281bde896ed
Tushar Goel <34160672+TG1999@users.noreply.github.com>
2023-11-27 12:20:09 +0530

    Add middleware to ban bytedance user agent (#1347)
    * Add middleware to ban bytedance user agent
    
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>
    
    * Change response type
    
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>
    
    ---------
    
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>

--- vulnerablecode/settings.py
+++ vulnerablecode/settings.py
@@ -89,6 +89,7 @@ MIDDLEWARE = (
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "vulnerabilities.middleware.ban_user_agent.BanUserAgent",
 )
 
 ROOT_URLCONF = "vulnerablecode.urls"

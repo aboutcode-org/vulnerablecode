d9e0f4fa75b4b65e9e0fafe87c1d5a4c311e7cca
Tushar Goel <tushar.goel.dav@gmail.com>
2023-12-26 20:48:33 +0530

    Refactor ubuntu usn
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>

--- vulnerabilities/importers/ubuntu_usn.py
+++ vulnerabilities/importers/ubuntu_usn.py
@@ -72,6 +72,9 @@ class UbuntuUSNImporter(Importer):
         for usn in usn_db:
             usn_data = usn_db[usn]
             usn_reference = get_usn_reference(usn_data.get("id"))
+            usn_references = []
+            if usn_reference:
+                usn_references = [usn_reference]
             for cve in usn_data.get("cves", []):
                 # The db sometimes contains entries like
                 # {'cves': ['python-pgsql vulnerabilities', 'CVE-2006-2313', 'CVE-2006-2314']}
@@ -82,14 +85,14 @@ class UbuntuUSNImporter(Importer):
                 yield AdvisoryData(
                     aliases=[cve],
                     summary="",
-                    references=[usn_reference],
+                    references=usn_references,
                     url=usn_reference.url or self.db_url,
                 )
 
 
 def get_usn_reference(usn_id):
     if not usn_id:
-        return []
+        return None
     return Reference(reference_id=f"USN-{usn_id}", url=f"https://usn.ubuntu.com/{usn_id}/")
 
 

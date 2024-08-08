8bc04450d9fbca28ab30058e728214883f1ccf40
Michael Ehab Mikhail <michael.ehab@hotmail.com>
2024-08-08 20:38:46 +0300

    -Refactor GitlabDataSource to improve code readability and performance
    -Update Gitlab Datasource tests
    
    -Reorder test_parse_interesting_advisories test files
    
    -Format changed files with black
    
    -Merge fetch yml logic and update method doc
    
    Signed-off-by: Michael Ehab Mikhail <michael.ehab@hotmail.com>

--- vulntotal/tests/test_gitlab.py
+++ vulntotal/tests/test_gitlab.py
@@ -1,4 +1,3 @@
-#
 # Copyright (c) nexB Inc. and others. All rights reserved.
 # VulnerableCode is a trademark of nexB Inc.
 # SPDX-License-Identifier: Apache-2.0
@@ -8,6 +7,7 @@
 #
 
 from pathlib import Path
+from unittest import mock
 
 from commoncode import testcase
 from packageurl import PackageURL
@@ -32,17 +32,35 @@ class TestGitlab(testcase.FileBasedTesting):
         expected_file = self.get_test_loc("package_advisory_url-expected.json", must_exist=False)
         util_tests.check_results_against_json(results, expected_file)
 
-    def test_parse_html_advisory(self):
+    @mock.patch("vulntotal.datasources.gitlab.fetch_yaml")
+    def test_parse_interesting_advisories(self, mock_fetch_yaml):
+        # Mock the yaml file responses
         advisory_folder = (
             Path(__file__)
             .resolve()
-            .parent.joinpath("test_data/gitlab/temp_vulntotal_gitlab_datasource")
-        )
-        results = [
-            adv.to_dict()
-            for adv in gitlab.parse_interesting_advisories(
-                advisory_folder, PackageURL("generic", "namespace", "test", "0.1.1"), False
+            .parent.joinpath(
+                "test_data/gitlab/temp_vulntotal_gitlab_datasource/gemnasium-db-master-pypi-Jinja2/pypi/Jinja2"
             )
+        )
+        yaml_files = []
+        sorted_files = sorted(advisory_folder.iterdir(), key=lambda x: x.name)
+        for file in sorted_files:
+            if file.suffix == ".yml":
+                with open(file, "r") as f:
+                    yaml_files.append(f.read())
+
+        mock_fetch_yaml.side_effect = yaml_files
+
+        purl = PackageURL("generic", "namespace", "test", "0.1.1")
+
+        yml_files = [
+            {"name": "CVE-2014-1402.yml", "path": "path/to/CVE-2014-1402.yml"},
+            {"name": "CVE-2016-10745.yml", "path": "path/to/CVE-2016-10745.yml"},
+            {"name": "CVE-2019-10906.yml", "path": "path/to/CVE-2019-10906.yml"},
+            {"name": "CVE-2020-28493.yml", "path": "path/to/CVE-2020-28493.yml"},
         ]
+
+        results = [adv.to_dict() for adv in gitlab.parse_interesting_advisories(yml_files, purl)]
+
         expected_file = self.get_test_loc("parsed_advisory-expected.json", must_exist=False)
         util_tests.check_results_against_json(results, expected_file)

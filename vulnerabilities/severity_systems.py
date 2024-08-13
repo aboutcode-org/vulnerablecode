91eefdbe72dba61a1689dc7c9ffc763b85dd10ff
ziad hany <ziadhany2016@gmail.com>
2024-08-13 20:18:31 +0300

    Add support for CVSSv4 & SSVC and import the data using vulnrichment (#1484)
    Add vulnrichment importer.
    Add support to CVSSv4
    Add vulnrichment importer.
    Add Support for SSVC.
    Add SSVC calculator.
    Ignore affected packages
    Add support for ref cpes & fix the tests
    Fix ssvc calculator bug
    
    Signed-off-by: ziadhany <ziadhany2016@gmail.com>Signed-off-by: Shrish Mishra shrish409@gmail.com

--- vulnerabilities/severity_systems.py
+++ vulnerabilities/severity_systems.py
@@ -8,9 +8,11 @@
 #
 
 import dataclasses
+from datetime import datetime
 
 from cvss import CVSS2
 from cvss import CVSS3
+from cvss import CVSS4
 
 """
 Vulnerability scoring systems define scales, values and approach to score a
@@ -83,6 +85,22 @@ class Cvssv3ScoringSystem(ScoringSystem):
         return CVSS3(vector=scoring_elements).as_json()
 
 
+@dataclasses.dataclass(order=True)
+class Cvssv4ScoringSystem(ScoringSystem):
+    def compute(self, scoring_elements: str) -> str:
+        """
+        Return a CVSSv4 base score
+
+        >>> CVSSV4.compute('CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:H/VI:H/VA:H/SC:H/SI:H/SA:N')
+        '9.9'
+        """
+        return str(CVSS4(vector=scoring_elements).base_score)
+
+    def get(self, scoring_elements: str) -> dict:
+        scoring_elements = scoring_elements.strip()
+        return CVSS4(vector=scoring_elements).as_json()
+
+
 CVSSV3 = Cvssv3ScoringSystem(
     identifier="cvssv3",
     name="CVSSv3 Base Score",
@@ -95,6 +113,13 @@ CVSSV31 = Cvssv3ScoringSystem(
     name="CVSSv3.1 Base Score",
     url="https://www.first.org/cvss/v3-1/",
     notes="CVSSv3.1 base score and vector",
+)
+
+CVSSV4 = Cvssv4ScoringSystem(
+    identifier="cvssv4",
+    name="CVSSv4 Base Score",
+    url="https://www.first.org/cvss/v4-0/",
+    notes="CVSSv4 base score and vector",
 )
 
 REDHAT_BUGZILLA = ScoringSystem(
@@ -170,12 +195,26 @@ EPSS = EPSSScoringSystem(
     url="https://www.first.org/epss/",
 )
 
+
+@dataclasses.dataclass(order=True)
+class SSVCScoringSystem(ScoringSystem):
+    def get(self, scoring_elements: str):
+        return {"version": "ssvc", "vectorString": scoring_elements}
+
+
+SSVC = SSVCScoringSystem(
+    identifier="ssvc",
+    name="Stakeholder-Specific Vulnerability Categorization",
+    url="https://www.cisa.gov/stakeholder-specific-vulnerability-categorization-ssvc",
+)
+
 SCORING_SYSTEMS = {
     system.identifier: system
     for system in (
         CVSSV2,
         CVSSV3,
         CVSSV31,
+        CVSSV4,
         REDHAT_BUGZILLA,
         REDHAT_AGGREGATE,
         ARCHLINUX,
@@ -184,5 +223,6 @@ SCORING_SYSTEMS = {
         APACHE_HTTPD,
         APACHE_TOMCAT,
         EPSS,
+        SSVC,
     )
 }

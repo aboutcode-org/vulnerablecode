caf6a70e21446aa65cd5d6f3f6f8239dc4ae760a
Keshav Priyadarshi <git@keshav.space>
2024-08-23 18:02:21 +0530

    Use paginated queryset for better memory performance
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>
    Signed-off-by: Shrish Mishra shrish409@gmail.com

--- vulnerabilities/pipelines/flag_ghost_packages.py
+++ vulnerabilities/pipelines/flag_ghost_packages.py
@@ -8,10 +8,11 @@
 #
 
 import logging
+from itertools import groupby
 from traceback import format_exc as traceback_format_exc
 
 from aboutcode.pipeline import LoopProgress
-from fetchcode.package_versions import SUPPORTED_ECOSYSTEMS
+from fetchcode.package_versions import SUPPORTED_ECOSYSTEMS as FETCHCODE_SUPPORTED_ECOSYSTEMS
 from fetchcode.package_versions import versions
 from packageurl import PackageURL
 from univers.version_range import RANGE_CLASS_BY_SCHEMES
@@ -32,27 +33,31 @@ class FlagGhostPackagePipeline(VulnerableCodePipeline):
 
 
 def detect_and_flag_ghost_packages(logger=None):
-    """Use fetchcode to validate the package indeed exists upstream."""
+    """Check if packages are available upstream. If not, mark them as ghost package."""
     interesting_packages_qs = (
-        Package.objects.filter(type__in=SUPPORTED_ECOSYSTEMS)
+        Package.objects.order_by("type", "namespace", "name")
+        .filter(type__in=FETCHCODE_SUPPORTED_ECOSYSTEMS)
         .filter(qualifiers="")
         .filter(subpath="")
     )
 
-    distinct_packages = interesting_packages_qs.values("type", "namespace", "name").distinct(
-        "type", "namespace", "name"
+    distinct_packages_count = (
+        interesting_packages_qs.values("type", "namespace", "name")
+        .distinct("type", "namespace", "name")
+        .count()
     )
 
-    distinct_packages_count = distinct_packages.count()
-    package_iterator = distinct_packages.iterator(chunk_size=2000)
-    progress = LoopProgress(total_iterations=distinct_packages_count, logger=logger)
+    grouped_packages = groupby(
+        interesting_packages_qs.paginated(),
+        key=lambda pkg: (pkg.type, pkg.namespace, pkg.name),
+    )
 
     ghost_package_count = 0
-
-    for package in progress.iter(package_iterator):
+    progress = LoopProgress(total_iterations=distinct_packages_count, logger=logger)
+    for type_namespace_name, packages in progress.iter(grouped_packages):
         ghost_package_count += flag_ghost_package(
-            package_dict=package,
-            interesting_packages_qs=interesting_packages_qs,
+            base_purl=PackageURL(*type_namespace_name),
+            packages=packages,
             logger=logger,
         )
 
@@ -60,46 +65,44 @@ def detect_and_flag_ghost_packages(logger=None):
         logger(f"Successfully flagged {ghost_package_count:,d} ghost Packages")
 
 
-def flag_ghost_package(package_dict, interesting_packages_qs, logger=None):
+def flag_ghost_package(base_purl, packages, logger=None):
     """
-    Check if all the versions of the package described by `package_dict` (type, namespace, name)
-    are available upstream. If they are not available, update the status to 'ghost'.
-    Otherwise, update the status to 'valid'.
+    Check if all the versions of the `purl` are available upstream.
+    If they are not available, update the `is_ghost` to `True`.
     """
-    if not package_dict["type"] in RANGE_CLASS_BY_SCHEMES:
+    if not base_purl.type in RANGE_CLASS_BY_SCHEMES:
         return 0
 
-    known_versions = get_versions(**package_dict, logger=logger)
-    if not known_versions:
+    known_versions = get_versions(purl=base_purl, logger=logger)
+    # Skip if encounter error while fetching known versions
+    if known_versions is None:
         return 0
-
-    version_class = RANGE_CLASS_BY_SCHEMES[package_dict["type"]].version_class
-    package_versions = interesting_packages_qs.filter(**package_dict).filter(status="unknown")
 
     ghost_packages = 0
-    for pkg in package_versions:
+    version_class = RANGE_CLASS_BY_SCHEMES[base_purl.type].version_class
+    for pkg in packages:
+        pkg.is_ghost = False
         if version_class(pkg.version) not in known_versions:
-            pkg.status = "ghost"
-            pkg.save()
+            pkg.is_ghost = True
             ghost_packages += 1
 
-    valid_package_versions = package_versions.exclude(status="ghost")
-    valid_package_versions.update(status="valid")
+            if logger:
+                logger(f"Flagging ghost package {pkg.purl!s}", level=logging.DEBUG)
+        pkg.save()
 
     return ghost_packages
 
 
-def get_versions(type, namespace, name, logger=None):
-    """Return set of known versions for the given package type, namespace, and name."""
-    versionless_purl = PackageURL(type=type, namespace=namespace, name=name)
-    version_class = RANGE_CLASS_BY_SCHEMES[type].version_class
+def get_versions(purl, logger=None):
+    """Return set of known versions for the given purl."""
+    version_class = RANGE_CLASS_BY_SCHEMES[purl.type].version_class
 
     try:
-        return {version_class(v.value) for v in versions(str(versionless_purl))}
+        return {version_class(v.value) for v in versions(str(purl))}
     except Exception as e:
         if logger:
             logger(
-                f"Error while fetching known versions for {versionless_purl!r}: {e!r} \n {traceback_format_exc()}",
+                f"Error while fetching known versions for {purl!s}: {e!r} \n {traceback_format_exc()}",
                 level=logging.ERROR,
             )
         return

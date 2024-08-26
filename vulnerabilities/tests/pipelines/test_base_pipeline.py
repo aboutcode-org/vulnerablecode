b2a57f6e887547dd79bcf75a913cf9ad1fbf2e86
Keshav Priyadarshi <git@keshav.space>
2024-08-27 01:19:28 +0530

    Inline test fixtures
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>
    Signed-off-by: Shrish Mishra shrish409@gmail.com

--- vulnerabilities/tests/pipelines/test_base_pipeline.py
+++ vulnerabilities/tests/pipelines/test_base_pipeline.py
@@ -10,11 +10,41 @@
 from unittest.mock import patch
 
 from django.test import TestCase
+from django.utils import timezone
+from packageurl import PackageURL
+from univers.version_range import VersionRange
 
 from vulnerabilities import models
+from vulnerabilities.importer import AdvisoryData
+from vulnerabilities.importer import AffectedPackage
+from vulnerabilities.importer import Reference
 from vulnerabilities.pipelines import VulnerableCodeBaseImporterPipeline
-from vulnerabilities.tests import advisory_data1
-from vulnerabilities.tests import get_advisory1
+
+advisory_data1 = AdvisoryData(
+    aliases=["CVE-2020-13371337"],
+    summary="vulnerability description here",
+    affected_packages=[
+        AffectedPackage(
+            package=PackageURL(type="pypi", name="dummy"),
+            affected_version_range=VersionRange.from_string("vers:pypi/>=1.0.0|<=2.0.0"),
+        )
+    ],
+    references=[Reference(url="https://example.com/with/more/info/CVE-2020-13371337")],
+    date_published=timezone.now(),
+    url="https://test.com",
+)
+
+
+def get_advisory1(created_by="test_pipeline"):
+    return models.Advisory.objects.create(
+        aliases=advisory_data1.aliases,
+        summary=advisory_data1.summary,
+        affected_packages=[pkg.to_dict() for pkg in advisory_data1.affected_packages],
+        references=[ref.to_dict() for ref in advisory_data1.references],
+        url=advisory_data1.url,
+        created_by=created_by,
+        date_collected=timezone.now(),
+    )
 
 
 class TestVulnerableCodeBaseImporterPipeline(TestCase):

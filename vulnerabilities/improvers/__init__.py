fbde42ffca958a088bcc65d165657d204d2b8970
ziadhany <ziadhany2016@gmail.com>
2024-09-17 17:59:38 +0300

    Resolve migration conflicts. Address the exploit in the API extension.
    Signed-off-by: ziadhany <ziadhany2016@gmail.com>

--- vulnerabilities/improvers/__init__.py
+++ vulnerabilities/improvers/__init__.py
@@ -8,8 +8,10 @@
 #
 
 from vulnerabilities.improvers import valid_versions
-from vulnerabilities.improvers import vulnerability_kev
 from vulnerabilities.improvers import vulnerability_status
+from vulnerabilities.pipelines import enhance_with_exploitdb
+from vulnerabilities.pipelines import enhance_with_kev
+from vulnerabilities.pipelines import enhance_with_metasploit
 from vulnerabilities.pipelines import flag_ghost_packages
 
 IMPROVERS_REGISTRY = [
@@ -30,8 +32,10 @@ IMPROVERS_REGISTRY = [
     valid_versions.GithubOSVImprover,
     vulnerability_status.VulnerabilityStatusImprover,
     valid_versions.CurlImprover,
-    vulnerability_kev.VulnerabilityKevImprover,
     flag_ghost_packages.FlagGhostPackagePipeline,
+    enhance_with_kev.VulnerabilityKevPipeline,
+    enhance_with_metasploit.MetasploitImproverPipeline,
+    enhance_with_exploitdb.ExploitDBImproverPipeline,
 ]
 
 IMPROVERS_REGISTRY = {x.qualified_name: x for x in IMPROVERS_REGISTRY}

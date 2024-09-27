2c2dfff58cdf266ad48ef789de71edc7f274764f
Keshav Priyadarshi <git@keshav.space>
2024-09-28 01:17:09 +0530

    Add data migration for old nvd advisory
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>

--- vulnerabilities/tests/test_data_migrations.py
+++ vulnerabilities/tests/test_data_migrations.py
@@ -802,3 +802,42 @@ class TestUpdateGitHubAdvisoryCreatedByField(TestMigrations):
             adv.filter(created_by="vulnerabilities.importers.github.GitHubAPIImporter").count() == 0
         )
         assert adv.filter(created_by="github_importer").count() == 1
+
+
+class TestUpdateNVDAdvisoryCreatedByField(TestMigrations):
+    app_name = "vulnerabilities"
+    migrate_from = "0067_update_github_advisory_created_by"
+    migrate_to = "0068_update_nvd_advisory_created_by"
+
+    advisory_data1 = AdvisoryData(
+        aliases=["CVE-2020-13371337"],
+        summary="vulnerability description here",
+        affected_packages=[
+            AffectedPackage(
+                package=PackageURL(type="pypi", name="foobar"),
+                affected_version_range=VersionRange.from_string("vers:pypi/>=1.0.0|<=2.0.0"),
+            )
+        ],
+        references=[Reference(url="https://example.com/with/more/info/CVE-2020-13371337")],
+        date_published=timezone.now(),
+        url="https://test.com",
+    )
+
+    def setUpBeforeMigration(self, apps):
+        Advisory = apps.get_model("vulnerabilities", "Advisory")
+        adv1 = Advisory.objects.create(
+            aliases=self.advisory_data1.aliases,
+            summary=self.advisory_data1.summary,
+            affected_packages=[pkg.to_dict() for pkg in self.advisory_data1.affected_packages],
+            references=[ref.to_dict() for ref in self.advisory_data1.references],
+            url=self.advisory_data1.url,
+            created_by="vulnerabilities.importers.nvd.NVDImporter",
+            date_collected=timezone.now(),
+        )
+
+    def test_removal_of_duped_purls(self):
+        Advisory = apps.get_model("vulnerabilities", "Advisory")
+        adv = Advisory.objects.all()
+
+        assert adv.filter(created_by="vulnerabilities.importers.nvd.NVDImporter").count() == 0
+        assert adv.filter(created_by="nvd_importer").count() == 1

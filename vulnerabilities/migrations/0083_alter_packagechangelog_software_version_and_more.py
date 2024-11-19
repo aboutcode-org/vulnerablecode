b28c7e67781155a80b45b223d140a8223020d238
Keshav Priyadarshi <git@keshav.space>
2024-11-19 10:58:13 +0530

    Resolve migration conflict
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>
    Signed-off-by: Shrish Mishra shrish409@gmail.com

--- vulnerabilities/migrations/0083_alter_packagechangelog_software_version_and_more.py
+++ vulnerabilities/migrations/0083_alter_packagechangelog_software_version_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("vulnerabilities", "0081_alter_packagechangelog_software_version_and_more"),
+        ("vulnerabilities", "0082_vulnerability_exploitability_and_more"),
     ]
 
     operations = [

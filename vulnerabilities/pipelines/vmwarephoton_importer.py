dcd7fcd0c57908e6d71b99d6179a5d6ea162ce11
Samk <sampurnapyne1710@gmail.com>
2024-11-25 01:09:48 +0530

    added to pipelines/__init__.py

--- vulnerabilities/pipelines/vmwarephoton_importer.py
+++ vulnerabilities/pipelines/vmwarephoton_importer.py
@@ -1,12 +1,3 @@
-#
-# Copyright (c) nexB Inc. and others. All rights reserved.
-# VulnerableCode is a trademark of nexB Inc.
-# SPDX-License-Identifier: Apache-2.0
-# See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
-# See https://github.com/aboutcode-org/vulnerablecode for support or download.
-# See https://aboutcode.org for more information about nexB OSS projects.
-#
-
 import gzip
 import json
 import logging
@@ -31,39 +22,11 @@ class VMWAREPHOTONImporterPipeline(VulnerableCodeBaseImporterPipeline):
     """Collect advisories from VMWARE_PHOTON."""
 
     pipeline_id = "vmwarephoton_importer"
+    repo_url = "https://github.com/vmware/photon/wiki/Security-Advisories"
+    spdx_license_expression = "CC BY-SA 4.0"
+    license_url = "https://creativecommons.org/licenses/by-sa/4.0/"
 
-    # See https://github.com/nexB/vulnerablecode/issues/36 for follow up
-    spdx_license_expression = (
-        "LicenseRef-scancode-us-govt-public-domain  AND LicenseRef-scancode-cve-tou"
-    )
-    license_url = "https://nvd.nist.gov/general/FAQ-Sections/General-FAQs#faqLink7"
-    """
-    EXAMPLE:
-        {
-            "cve_id": "CVE-2020-11979",
-            "pkg": "apache-ant",
-            "cve_score": 7.5,
-            "aff_ver": "all versions before 1.10.8-2.ph1 are vulnerable",
-            "res_ver": "1.10.8-2.ph1"
-        },
-        {
-            "cve_id": "CVE-2020-1945",
-            "pkg": "apache-ant",
-            "cve_score": 6.3,
-            "aff_ver": "all versions before 1.10.8-1.ph1 are vulnerable",
-            "res_ver": "1.10.8-1.ph1"
-        },
-        {
-            "cve_id": "CVE-2021-36373",
-            "pkg": "apache-ant",
-            "cve_score": 5.5,
-            "aff_ver": "all versions before 1.10.8-4.ph1 are vulnerable",
-            "res_ver": "1.10.8-4.ph1"
-        }
-    """
     importer_name = "PHOTON Importer"
-
-    
 
     def advisories_count(self):
         url = "https://packages.vmware.com/photon/photon_cve_metadata/cve_data_photon1.0.json"
@@ -82,33 +45,41 @@ class VMWAREPHOTONImporterPipeline(VulnerableCodeBaseImporterPipeline):
 
         advisory_count = len(data)
         return advisory_count
-    
 
-    
     def collect_advisories(self) -> Iterable[AdvisoryData]:
-        if VMWAREPHOTONImporterPipeline.advisories_count():
-            VMWAREPHOTONImporterPipeline.to_advisory(self.data)
-
-
-    def to_advisory(data):
-        advisories=[]
-        for cve in data:
-            cve_id=cve.get("cve_id")
-            pkg=cve.get("pkg")
-            cve_scoore=cve.get("cve_score")
-            aff_ver=cve.get("aff_ver")
-            rev_ver=cve.get("res_ver")
-
-        """
-        Returns an AdvisoryData object from this CVE item and adds it to the advisory list
-        """
-        advisories.append(
-             AdvisoryData(
-                 
-                aliases=cve_id,
-                affected_packages=pkg,
-                
+        # Fetch advisory data from the URL
+        url = "https://packages.vmware.com/photon/photon_cve_metadata/cve_data_photon1.0.json"
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            advisories_data = response.json()  # Fetch the data from the API
+        except requests.HTTPError as http_err:
+            self.log(
+                f"HTTP error occurred: {http_err} \n {traceback_format_exc()}",
+                level=logging.ERROR,
             )
-        )
+            return []
+
+        # Pass the fetched data to the to_advisory method
+        advisories = self.to_advisory(advisories_data)
         return advisories
 
+    def to_advisory(self, data) -> Iterable[AdvisoryData]:
+        advisories = []
+        for cve in data:
+            cve_id = cve.get("cve_id")
+            pkg = cve.get("pkg")
+            cve_score = cve.get("cve_score")
+            aff_ver = cve.get("aff_ver")
+            rev_ver = cve.get("res_ver")
+
+            advisories.append(
+                AdvisoryData(
+                    aliases=[cve_id],  # Pass cve_id as aliases
+                    affected_packages=[pkg],  # Package list
+                    # cve_score = [cve_score],
+                    # aff_ver = [aff_ver],
+                    # rev_ver = [rev_ver]
+                )
+            )
+        return advisories

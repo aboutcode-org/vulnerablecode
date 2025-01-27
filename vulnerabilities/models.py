333c874217ad4123d97d32c3fa5891374bfc4237
Keshav Priyadarshi <git@keshav.space>
2025-01-27 23:36:08 +0530

    Add unique_together constraint for AdvisoryToDo
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>

--- vulnerabilities/models.py
+++ vulnerabilities/models.py
@@ -1801,25 +1801,36 @@ class CodeFix(CodeChange):
     )
 
 
-class AdvisoryTODO(models.Model):
+ISSUE_TYPE_CHOICES = [
+    ("MISSING_AFFECTED_PACKAGE", "Advisory is missing affected package"),
+    ("MISSING_FIXED_BY_PACKAGE", "Advisory is missing fixed-by package"),
+    (
+        "MISSING_AFFECTED_AND_FIXED_BY_PACKAGES",
+        "Advisory is missing both affected and fixed-by packages",
+    ),
+    ("MISSING_SUMMARY", "Advisory is missing summary"),
+    ("CONFLICTING_FIXED_BY_PACKAGES", "Advisories have conflicting fixed-by packages"),
+    ("CONFLICTING_AFFECTED_PACKAGES", "Advisories have conflicting affected packages"),
+    (
+        "CONFLICTING_AFFECTED_AND_FIXED_BY_PACKAGES",
+        "Advisories have conflicting affected and fixed-by packages",
+    ),
+    ("CONFLICTING_SEVERITY_SCORES", "Advisories have conflicting severity scores"),
+]
+
+
+class AdvisoryToDo(models.Model):
     """Track the TODOs for advisory/ies that need to be addressed."""
 
-    ISSUE_TYPE_CHOICES = [
-        ("MISSING_AFFECTED_PACKAGE", "Advisory is missing affected package"),
-        ("MISSING_FIXED_BY_PACKAGE", "Advisory is missing fixed-by package"),
-        (
-            "MISSING_AFFECTED_AND_FIXED_BY_PACKAGES",
-            "Advisory is missing both affected and fixed-by packages",
-        ),
-        ("MISSING_SUMMARY", "Advisory is missing summary"),
-        ("CONFLICTING_FIXED_BY_PACKAGES", "Advisories have conflicting fixed-by packages"),
-        ("CONFLICTING_AFFECTED_PACKAGES", "Advisories have conflicting affected packages"),
-        (
-            "CONFLICTING_AFFECTED_AND_FIXED_BY_PACKAGES",
-            "Advisories have conflicting affected and fixed-by packages",
-        ),
-        ("CONFLICTING_SEVERITY_SCORES", "Advisories have conflicting severity scores"),
-    ]
+    # Since we can not make advisories field (M2M field) unique
+    # (see https://code.djangoproject.com/ticket/702), we use related_advisories_id
+    # to avoid creating duplicate issue for same set of advisories,
+    related_advisories_id = models.CharField(
+        max_length=40,
+        blank=False,
+        null=False,
+        help_text="SHA1 digest of the unique_content_id field of the applicable advisories.",
+    )
 
     issue_type = models.CharField(
         max_length=50,
@@ -1829,9 +1840,11 @@ class AdvisoryTODO(models.Model):
         db_index=True,
         help_text="Select the issue that needs to be addressed from the available options.",
     )
+
     issue_detail = models.TextField(
         help_text="Additional details about the issue.",
     )
+
     advisories = models.ManyToManyField(
         Advisory,
         related_name="advisory_todos",
@@ -1856,3 +1869,6 @@ class AdvisoryTODO(models.Model):
     resolution_detail = models.TextField(
         help_text="Additional detail on how this TODO was resolved.",
     )
+
+    class Meta:
+        unique_together = ("related_advisories_id", "issue_type")

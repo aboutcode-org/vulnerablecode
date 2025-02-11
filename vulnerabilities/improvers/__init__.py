fcd30033575a9f80b4e81e78cdc977248b8b331d
Tushar Goel <tushar.goel.dav@gmail.com>
2025-02-11 19:39:41 +0530

    Adress review comments
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>

--- vulnerabilities/improvers/__init__.py
+++ vulnerabilities/improvers/__init__.py
@@ -17,6 +17,7 @@ from vulnerabilities.pipelines import compute_package_version_rank
 from vulnerabilities.pipelines import enhance_with_exploitdb
 from vulnerabilities.pipelines import enhance_with_kev
 from vulnerabilities.pipelines import enhance_with_metasploit
+from vulnerabilities.pipelines import fill_vulnerability_summary_pipeline
 from vulnerabilities.pipelines import flag_ghost_packages
 from vulnerabilities.pipelines import remove_duplicate_advisories
 
@@ -47,6 +48,7 @@ IMPROVERS_REGISTRY = [
     collect_commits.CollectFixCommitsPipeline,
     add_cvss31_to_CVEs.CVEAdvisoryMappingPipeline,
     remove_duplicate_advisories.RemoveDuplicateAdvisoriesPipeline,
+    fill_vulnerability_summary_pipeline.FillVulnerabilitySummariesPipeline,
 ]
 
 IMPROVERS_REGISTRY = {

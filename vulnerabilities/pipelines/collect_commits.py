94241a0dfccd087ecd74cc2562b5a8a1f606588a
Shrish0098 <shrish409@gmail.com>
2025-03-08 10:47:07 +0530

    Removed paginated() and used interator()
    Signed-off-by: Shrish0098 <shrish409@gmail.com>

--- vulnerabilities/pipelines/collect_commits.py
+++ vulnerabilities/pipelines/collect_commits.py
@@ -51,9 +51,8 @@ class CollectFixCommitsPipeline(VulnerableCodePipeline):
             total_iterations=affected_by_package_related_vulnerabilities.count(), logger=self.log
         )
 
-        for apv in progress.iter(
-            affected_by_package_related_vulnerabilities.paginated(per_page=500)
-        ):
+        # Remove the per_page parameter
+        for apv in progress.iter(affected_by_package_related_vulnerabilities.iterator()):
             vulnerability = apv.vulnerability
             for reference in vulnerability.references.all():
                 if not "/commit/" in reference.url:

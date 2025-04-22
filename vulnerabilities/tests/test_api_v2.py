d9972689c29e963e485aff579116e61c718e6056
RISHI GARG <134256793+Rishi-source@users.noreply.github.com>
2025-04-22 16:04:13 +0530

    Fix: Workflow fail for apiv2 test
    Signed-off-by: RISHI GARG <134256793+Rishi-source@users.noreply.github.com>

--- vulnerabilities/tests/test_api_v2.py
+++ vulnerabilities/tests/test_api_v2.py
@@ -92,22 +92,6 @@ class VulnerabilityV2ViewSetTest(APITestCase):
         self.assertEqual(len(response.data["weaknesses"]), 1)
         self.assertEqual(len(response.data["references"]), 1)
 
-    def test_filter_packages_by_vulnerability_status(self):
-        vulnerability = Vulnerability.objects.create(
-            vulnerability_id="VCID-FILTER", summary="Test vulnerability for is_vulnerable filter"
-        )
-        self.package1.affected_by_vulnerabilities.add(vulnerability)
-        url = reverse("package-v2-list")
-        with self.assertNumQueries(20):
-            response = self.client.get(url, {"is_vulnerable": "true"}, format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data["results"]["packages"]), 1)
-        self.assertEqual(response.data["results"]["packages"][0]["purl"], "pkg:pypi/django@3.2")
-        with self.assertNumQueries(20):
-            response = self.client.get(url, {"is_vulnerable": "false"}, format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data["results"]["packages"]), 1)
-        self.assertEqual(response.data["results"]["packages"][0]["purl"], "pkg:npm/lodash@4.17.20")
 
     def test_filter_vulnerability_by_vulnerability_id(self):
         """
@@ -249,6 +233,22 @@ class PackageV2ViewSetTest(APITestCase):
         self.assertTrue(
             all(vuln_id in response.data["results"]["vulnerabilities"] for vuln_id in package_vulns)
         )
+    def test_filter_packages_by_vulnerability_status(self):
+        vulnerability = Vulnerability.objects.create(
+            vulnerability_id="VCID-FILTER", summary="Test vulnerability for is_vulnerable filter"
+        )
+        self.package1.affected_by_vulnerabilities.add(vulnerability)
+        url = reverse("package-v2-list")
+        with self.assertNumQueries(20):
+            response = self.client.get(url, {"is_vulnerable": "true"}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data["results"]["packages"]), 1)
+        self.assertEqual(response.data["results"]["packages"][0]["purl"], "pkg:pypi/django@3.2")
+        with self.assertNumQueries(20):
+            response = self.client.get(url, {"is_vulnerable": "false"}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data["results"]["packages"]), 1)
+        self.assertEqual(response.data["results"]["packages"][0]["purl"], "pkg:npm/lodash@4.17.20")
 
     def test_filter_packages_by_purl(self):
         """

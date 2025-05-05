efd8d2c0d057b98cc826a7ab5af08e38bd2850a7
Keshav Priyadarshi <git@keshav.space>
2025-05-05 23:22:23 +0530

    Add utility to get latest commit hash
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>

--- vulnerablecode/__init__.py
+++ vulnerablecode/__init__.py
@@ -9,8 +9,35 @@
 
 import os
 import sys
+from contextlib import suppress
+from pathlib import Path
+
+import git
 
 __version__ = "36.0.0"
+
+
+PROJECT_DIR = Path(__file__).resolve().parent
+ROOT_DIR = PROJECT_DIR.parent
+
+
+def get_git_describe_from_local_checkout():
+    """
+    Return the git describe tag from the local checkout.
+    This will only provide a result when the codebase is a git clone.
+    """
+    with suppress(git.GitError):
+        return git.Repo(".").git.describe(tags=True, always=True)
+
+
+def get_short_commit():
+    """
+    Return the short commit hash from a Git describe string while removing
+    any leading "g" character if present.
+    """
+    if git_describe := get_git_describe_from_local_checkout():
+        short_commit = git_describe.split("-")[-1]
+        return short_commit.lstrip("g")
 
 
 def command_line():

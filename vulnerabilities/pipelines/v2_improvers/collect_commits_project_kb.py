493e21022a42e8255b89feaa7a02fc2824ca8b58
ziad hany <ziadhany2016@gmail.com>
2025-10-09 15:56:41 +0300

    Add ProjectKBv2 importer Add a test for the ProjectKB importer and  collect fix commits pipeline.
    Signed-off-by: ziad hany <ziadhany2016@gmail.com>

--- vulnerabilities/pipelines/v2_improvers/collect_commits_project_kb.py
+++ vulnerabilities/pipelines/v2_improvers/collect_commits_project_kb.py
@@ -6,10 +6,10 @@
 # See https://github.com/aboutcode-org/vulnerablecode for support or download.
 # See https://aboutcode.org for more information about nexB OSS projects.
 #
+
 import csv
 from pathlib import Path
 
-import saneyaml
 from fetchcode.vcs import fetch_via_vcs
 
 from vulnerabilities.models import AdvisoryV2
@@ -21,100 +21,71 @@ class CollectFixCommitsProjectKBPipeline(VulnerableCodePipeline):
     """
     Pipeline to collect fix commits from Project KB:
     https://github.com/SAP/project-kb/blob/main/MSR2019/dataset/vulas_db_msr2019_release.csv
-    https://github.com/SAP/project-kb/blob/vulnerability-data/statements/*/*.yaml
     """
 
     pipeline_id = "kb_project_fix_commits"
     spdx_license_expression = "Apache-2.0"
     license_url = "https://github.com/SAP/project-kb/blob/main/LICENSE.txt"
-    importer_name = "Project KB Importer"
     qualified_name = "kb_project_fix_commits"
-    repo_url_vulnerability_data = "git+https://github.com/SAP/project-kb@vulnerability-data"
-    repo_url_main = "git+https://github.com/SAP/project-kb"
+    repo_url = "git+https://github.com/SAP/project-kb"
 
     @classmethod
     def steps(cls):
-        return (cls.collect_fix_commits,)
+        return (
+            cls.clone,
+            cls.collect_fix_commits,
+        )
+
+    def clone(self):
+        self.log("Cloning repositories for ProjectKB fix commits from CSV...")
+        self.vcs_response = fetch_via_vcs(self.repo_url)
 
     def collect_fix_commits(self):
-        self.vcs_response_main = fetch_via_vcs(self.repo_url_main)
-        self.vcs_response_vuln_data = fetch_via_vcs(self.repo_url_vulnerability_data)
+        self.log("Collecting fix commits from ProjectKB...")
 
-        self.log(f"Processing ProjectKBP fix commits.")
-        csv_database_filepath = (
-            Path(self.vcs_response_main.dest_dir) / "MSR2019/dataset/vulas_db_msr2019_release.csv"
+        csv_path = Path(self.vcs_response.dest_dir) / "MSR2019/dataset/vulas_db_msr2019_release.csv"
+
+        with open(csv_path, newline="", encoding="utf-8") as f:
+            reader = csv.reader(f)
+            next(reader, None)  # skip header
+            rows = [r for r in reader if len(r) == 4 and r[0]]
+
+        vuln_ids = {r[0] for r in rows}
+        advisories = AdvisoryV2.objects.filter(advisory_id__in=vuln_ids).prefetch_related(
+            "impacted_packages__affecting_packages"
         )
-        try:
-            with open(csv_database_filepath, mode="r", newline="", encoding="utf-8") as f:
-                reader = csv.reader(f)
-                next(reader, None)  # Skip header row
-                for row in reader:
-                    if len(row) != 4:
-                        continue
-                    vulnerability_id, repo_url, commit_hash, label = row
+        advisory_map = {a.advisory_id: a for a in advisories}
 
-                    if not vulnerability_id:
-                        continue
-
-                    try:
-                        advisory = AdvisoryV2.objects.get(advisory_id=vulnerability_id)
-                    except AdvisoryV2.DoesNotExist:
-                        self.log(f"Can't find vulnerability_id: {vulnerability_id}")
-                        continue
-
-                    self.create_codefix_entries(advisory, repo_url, commit_hash, vulnerability_id)
-        except FileNotFoundError:
-            self.log(f"CSV file not found: {csv_database_filepath}")
-
-        base_path = Path(self.vcs_response_vuln_data.dest_dir) / "statements"
-        for file_path in base_path.rglob("*.yaml"):
-            if file_path.name != "statement.yaml":
+        codefixes = []
+        for vuln_id, repo_url, commit, _ in rows:
+            advisory = advisory_map.get(vuln_id)
+            if not advisory:
                 continue
 
-            with open(file_path) as f:
-                vulnerability_fixes_data = saneyaml.load(f)
+            repo_url = repo_url.rstrip("/").removesuffix(".git")
+            vcs_url = f"{repo_url}/commit/{commit}"
 
-            vulnerability_id = vulnerability_fixes_data.get("vulnerability_id")
-            if not vulnerability_id:
-                continue
-
-            try:
-                advisory = AdvisoryV2.objects.get(advisory_id=vulnerability_id)
-            except AdvisoryV2.DoesNotExist:
-                self.log(f"Can't find vulnerability_id: {vulnerability_id}")
-                continue
-
-            for commit_data in vulnerability_fixes_data.get("fixes", []):
-                for commit in commit_data.get("commits", []):
-                    commit_id = commit.get("id")
-                    repo_url = commit.get("repository")
-
-                    if not commit_id or not repo_url:
-                        continue
-
-                    self.create_codefix_entries(advisory, repo_url, commit_id, vulnerability_id)
-
-    def create_codefix_entries(self, advisory, repo_url, commit_id, vulnerability_id):
-        repo_url = repo_url.rstrip("/").removesuffix(".git")
-        vcs_url = f"{repo_url}/commit/{commit_id}"
-
-        for impact in advisory.impacted_packages.all():
-            for package in impact.affecting_packages.all():
-                code_fix, created = CodeFixV2.objects.get_or_create(
-                    commits=[vcs_url],
-                    advisory=advisory,
-                    affected_package=package,
-                )
-                if created:
-                    self.log(
-                        f"Created CodeFix entry for vulnerability_id: {vulnerability_id} with VCS URL {vcs_url}"
+            for impact in advisory.impacted_packages.all():
+                for pkg in impact.affecting_packages.all():
+                    codefixes.append(
+                        CodeFixV2(
+                            commits=[vcs_url],
+                            advisory=advisory,
+                            affected_package=pkg,
+                        )
                     )
 
+        if codefixes:
+            CodeFixV2.objects.bulk_create(codefixes, ignore_conflicts=True)
+            self.log(f"Created {len(codefixes)} CodeFix entries.")
+        else:
+            self.log("No CodeFix entries created.")
+
     def clean_downloads(self):
-        if self.vcs_response_main or self.vcs_response_vuln_data:
+        """Remove the cloned repository from disk."""
+        if self.vcs_response:
             self.log(f"Removing cloned repository")
-            self.vcs_response_main.delete()
-            self.vcs_response_vuln_data.delete()
+            self.vcs_response.delete()
 
     def on_failure(self):
         self.clean_downloads()

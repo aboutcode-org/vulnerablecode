081a2451519c598f5c37d944e0144eaae3e6e5b6
ziad hany <ziadhany2016@gmail.com>
2026-01-13 01:54:45 +0200

    Fix code style
    Signed-off-by: ziad hany <ziadhany2016@gmail.com>

--- vulnerabilities/importers/__init__.py
+++ vulnerabilities/importers/__init__.py
@@ -54,8 +54,8 @@ from vulnerabilities.pipelines.v2_importers import (
     elixir_security_importer as elixir_security_importer_v2,
 )
 from vulnerabilities.pipelines.v2_importers import epss_importer_v2
-from vulnerabilities.pipelines.v2_importers import gentoo_importer as gentoo_importer_v2
 from vulnerabilities.pipelines.v2_importers import fireeye_importer_v2
+from vulnerabilities.pipelines.v2_importers import gentoo_importer as gentoo_importer_v2
 from vulnerabilities.pipelines.v2_importers import github_osv_importer as github_osv_importer_v2
 from vulnerabilities.pipelines.v2_importers import gitlab_importer as gitlab_importer_v2
 from vulnerabilities.pipelines.v2_importers import istio_importer as istio_importer_v2

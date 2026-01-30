8da445f6484ba46464822c90bc3be178db4bfd2c
ziad hany <ziadhany2016@gmail.com>
2026-01-30 03:50:30 +0200

    Update Alpine pipeline to use aboutcode-mirror-alpine-secdb
    Signed-off-by: ziad hany <ziadhany2016@gmail.com>

--- vulnerabilities/tests/pipelines/v2_importers/test_alpine_linux_importer_pipeline.py
+++ vulnerabilities/tests/pipelines/v2_importers/test_alpine_linux_importer_pipeline.py
@@ -19,10 +19,6 @@ from univers.versions import AlpineLinuxVersion
 from vulnerabilities.importer import AdvisoryData
 from vulnerabilities.importer import AffectedPackageV2
 from vulnerabilities.importer import ReferenceV2
-from vulnerabilities.pipelines.v2_importers.alpine_linux_importer import (
-    fetch_advisory_directory_links,
-)
-from vulnerabilities.pipelines.v2_importers.alpine_linux_importer import fetch_advisory_links
 from vulnerabilities.pipelines.v2_importers.alpine_linux_importer import load_advisories
 from vulnerabilities.pipelines.v2_importers.alpine_linux_importer import process_record
 from vulnerabilities.tests.pipelines import TestLogger
@@ -562,7 +558,7 @@ def test_process_record():
         ),
         AdvisoryData(
             advisory_id="xen/v3.11/4.10.0-r2/CVE-2018-7540",
-            aliases=["CVE-2018-7540"],
+            aliases=["CVE-2018-7540", "XSA-252"],
             summary="",
             affected_packages=[
                 AffectedPackageV2(
@@ -729,175 +725,6 @@ def test_process_record():
             url="https://secdb.alpinelinux.org/v3.11/",
             original_advisory_text=None,
         ),
-        AdvisoryData(
-            advisory_id="xen/v3.11/4.10.0-r2/XSA-252",
-            aliases=["XSA-252"],
-            summary="",
-            affected_packages=[
-                AffectedPackageV2(
-                    package=PackageURL(
-                        type="apk",
-                        namespace="alpine",
-                        name="xen",
-                        version=None,
-                        qualifiers={
-                            "arch": "aarch64",
-                            "distroversion": "v3.11",
-                            "reponame": "main",
-                        },
-                        subpath=None,
-                    ),
-                    affected_version_range=None,
-                    fixed_version_range=AlpineLinuxVersionRange(
-                        constraints=(
-                            VersionConstraint(
-                                comparator="=", version=AlpineLinuxVersion(string="4.10.0-r2")
-                            ),
-                        )
-                    ),
-                    introduced_by_commit_patches=[],
-                    fixed_by_commit_patches=[],
-                ),
-                AffectedPackageV2(
-                    package=PackageURL(
-                        type="apk",
-                        namespace="alpine",
-                        name="xen",
-                        version=None,
-                        qualifiers={"arch": "armhf", "distroversion": "v3.11", "reponame": "main"},
-                        subpath=None,
-                    ),
-                    affected_version_range=None,
-                    fixed_version_range=AlpineLinuxVersionRange(
-                        constraints=(
-                            VersionConstraint(
-                                comparator="=", version=AlpineLinuxVersion(string="4.10.0-r2")
-                            ),
-                        )
-                    ),
-                    introduced_by_commit_patches=[],
-                    fixed_by_commit_patches=[],
-                ),
-                AffectedPackageV2(
-                    package=PackageURL(
-                        type="apk",
-                        namespace="alpine",
-                        name="xen",
-                        version=None,
-                        qualifiers={"arch": "armv7", "distroversion": "v3.11", "reponame": "main"},
-                        subpath=None,
-                    ),
-                    affected_version_range=None,
-                    fixed_version_range=AlpineLinuxVersionRange(
-                        constraints=(
-                            VersionConstraint(
-                                comparator="=", version=AlpineLinuxVersion(string="4.10.0-r2")
-                            ),
-                        )
-                    ),
-                    introduced_by_commit_patches=[],
-                    fixed_by_commit_patches=[],
-                ),
-                AffectedPackageV2(
-                    package=PackageURL(
-                        type="apk",
-                        namespace="alpine",
-                        name="xen",
-                        version=None,
-                        qualifiers={
-                            "arch": "ppc64le",
-                            "distroversion": "v3.11",
-                            "reponame": "main",
-                        },
-                        subpath=None,
-                    ),
-                    affected_version_range=None,
-                    fixed_version_range=AlpineLinuxVersionRange(
-                        constraints=(
-                            VersionConstraint(
-                                comparator="=", version=AlpineLinuxVersion(string="4.10.0-r2")
-                            ),
-                        )
-                    ),
-                    introduced_by_commit_patches=[],
-                    fixed_by_commit_patches=[],
-                ),
-                AffectedPackageV2(
-                    package=PackageURL(
-                        type="apk",
-                        namespace="alpine",
-                        name="xen",
-                        version=None,
-                        qualifiers={"arch": "s390x", "distroversion": "v3.11", "reponame": "main"},
-                        subpath=None,
-                    ),
-                    affected_version_range=None,
-                    fixed_version_range=AlpineLinuxVersionRange(
-                        constraints=(
-                            VersionConstraint(
-                                comparator="=", version=AlpineLinuxVersion(string="4.10.0-r2")
-                            ),
-                        )
-                    ),
-                    introduced_by_commit_patches=[],
-                    fixed_by_commit_patches=[],
-                ),
-                AffectedPackageV2(
-                    package=PackageURL(
-                        type="apk",
-                        namespace="alpine",
-                        name="xen",
-                        version=None,
-                        qualifiers={"arch": "x86", "distroversion": "v3.11", "reponame": "main"},
-                        subpath=None,
-                    ),
-                    affected_version_range=None,
-                    fixed_version_range=AlpineLinuxVersionRange(
-                        constraints=(
-                            VersionConstraint(
-                                comparator="=", version=AlpineLinuxVersion(string="4.10.0-r2")
-                            ),
-                        )
-                    ),
-                    introduced_by_commit_patches=[],
-                    fixed_by_commit_patches=[],
-                ),
-                AffectedPackageV2(
-                    package=PackageURL(
-                        type="apk",
-                        namespace="alpine",
-                        name="xen",
-                        version=None,
-                        qualifiers={"arch": "x86_64", "distroversion": "v3.11", "reponame": "main"},
-                        subpath=None,
-                    ),
-                    affected_version_range=None,
-                    fixed_version_range=AlpineLinuxVersionRange(
-                        constraints=(
-                            VersionConstraint(
-                                comparator="=", version=AlpineLinuxVersion(string="4.10.0-r2")
-                            ),
-                        )
-                    ),
-                    introduced_by_commit_patches=[],
-                    fixed_by_commit_patches=[],
-                ),
-            ],
-            references=[],
-            references_v2=[
-                ReferenceV2(
-                    reference_id="XSA-252",
-                    reference_type="",
-                    url="https://xenbits.xen.org/xsa/advisory-252.html",
-                )
-            ],
-            patches=[],
-            date_published=None,
-            weaknesses=[],
-            severities=[],
-            url="https://secdb.alpinelinux.org/v3.11/",
-            original_advisory_text=None,
-        ),
     ]
 
     with open(TEST_DATA / "v3.11/main.json") as f:
@@ -913,60 +740,6 @@ def test_process_record():
         "'4.10-1-r1' is not a valid AlpineVersion InvalidVersion(\"'4.10-1-r1' is not a valid <class 'univers.versions.AlpineLinuxVersion'>\")"
         in logger.getvalue()
     )
-
-
-def test_fetch_advisory_directory_links():
-    expected = [
-        "https://secdb.alpinelinux.org/edge/",
-        "https://secdb.alpinelinux.org/v3.10/",
-        "https://secdb.alpinelinux.org/v3.11/",
-        "https://secdb.alpinelinux.org/v3.12/",
-        "https://secdb.alpinelinux.org/v3.13/",
-        "https://secdb.alpinelinux.org/v3.14/",
-        "https://secdb.alpinelinux.org/v3.15/",
-        "https://secdb.alpinelinux.org/v3.2/",
-        "https://secdb.alpinelinux.org/v3.3/",
-        "https://secdb.alpinelinux.org/v3.4/",
-        "https://secdb.alpinelinux.org/v3.5/",
-        "https://secdb.alpinelinux.org/v3.6/",
-        "https://secdb.alpinelinux.org/v3.7/",
-        "https://secdb.alpinelinux.org/v3.8/",
-        "https://secdb.alpinelinux.org/v3.9/",
-    ]
-    with open(TEST_DATA / "web_pages/directory.html") as f:
-        assert (
-            fetch_advisory_directory_links(f.read(), "https://secdb.alpinelinux.org/") == expected
-        )
-
-
-def test_fetch_advisory_directory_links_failure():
-    logger = TestLogger()
-    with open(TEST_DATA / "web_pages/fail_directory.html") as f:
-        assert (
-            fetch_advisory_directory_links(
-                f.read(), "https://secdb.alpinelinux.org/", logger=logger.write
-            )
-            == []
-        )
-        assert "No versions found in 'https://secdb.alpinelinux.org/'" in logger.getvalue()
-
-
-def test_fetch_advisory_links():
-    expected = [
-        "https://secdb.alpinelinux.org/v3.11/community.json",
-        "https://secdb.alpinelinux.org/v3.11/main.json",
-    ]
-    with open(TEST_DATA / "web_pages/v3.11.html") as f:
-        assert (
-            list(fetch_advisory_links(f.read(), "https://secdb.alpinelinux.org/v3.11/")) == expected
-        )
-
-
-def test_fetch_advisory_links_failure():
-    logger = TestLogger()
-    with open(TEST_DATA / "web_pages/fail_directory.html") as f:
-        assert list(fetch_advisory_links(f.read(), "v3.11", logger=logger.write)) == []
-        assert "No anchor tags found in 'v3.11'" in logger.getvalue()
 
 
 def test_process_record_without_packages():

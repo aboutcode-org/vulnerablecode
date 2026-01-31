ad451b41dc221c5cd045101efa6fb65c529fd420
ziad hany <ziadhany2016@gmail.com>
2026-01-31 18:10:40 +0200

    Update the target repositories list for parsing git commits logs
    Signed-off-by: ziad hany <ziadhany2016@gmail.com>

--- vulnerabilities/pipelines/v2_importers/collect_fix_commits.py
+++ vulnerabilities/pipelines/v2_importers/collect_fix_commits.py
@@ -1,71 +1,191 @@
 from vulnerabilities.pipelines import CollectVCSFixCommitPipeline
 
 
-class CollectNodejsFixCommitsPipeline(CollectVCSFixCommitPipeline):
-    pipeline_id = "collect_nodejs_fix_commits"
-    repo_url = "https://github.com/nodejs/node"
+class CollectLinuxFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_linux_fix_commits"
+    repo_url = "https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"
 
 
-class CollectCpythonFixCommitsPipeline(CollectVCSFixCommitPipeline):
-    pipeline_id = "collect_cpython_fix_commits"
-    repo_url = "https://github.com/python/cpython"
-
-
-class CollectGoFixCommitsPipeline(CollectVCSFixCommitPipeline):
-    pipeline_id = "collect_go_fix_commits"
-    repo_url = "https://github.com/golang/go"
-
-
-class CollectRustFixCommitsPipeline(CollectVCSFixCommitPipeline):
-    pipeline_id = "collect_rust_lang_fix_commits"
-    repo_url = "https://github.com/rust-lang/rust"
-
-
-class CollectPhpFixCommitsPipeline(CollectVCSFixCommitPipeline):
-    pipeline_id = "collect_php_fix_commits"
-    repo_url = "https://github.com/php/php-src"
-
-
-class CollectRubyFixCommitsPipeline(CollectVCSFixCommitPipeline):
-    pipeline_id = "collect_ruby_fix_commits"
-    repo_url = "https://github.com/ruby/ruby"
+class CollectBusyBoxFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_busybox_fix_commits"
+    repo_url = "https://github.com/mirror/busybox.git"
 
 
 class CollectNginxFixCommitsPipeline(CollectVCSFixCommitPipeline):
     pipeline_id = "collect_nginx_fix_commits"
-    repo_url = "https://github.com/nginx/nginx"
+    repo_url = "https://github.com/nginx/nginx.git"
 
 
-class CollectPostgresFixCommitsPipeline(CollectVCSFixCommitPipeline):
-    pipeline_id = "collect_postgres_fix_commits"
-    repo_url = "https://github.com/postgres/postgres"
+class CollectApacheTomcatFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_apache_tomcat_fix_commits"
+    repo_url = "https://github.com/apache/tomcat.git"
 
 
-class CollectMysqlFixCommitsPipeline(CollectVCSFixCommitPipeline):
-    pipeline_id = "collect_mysql_fix_commits"
-    repo_url = "https://github.com/mysql/mysql-server"
+class CollectMysqlServerFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_mysql_server_fix_commits"
+    repo_url = "https://github.com/mysql/mysql-server.git"
+
+
+class CollectPostgresqlFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_postgresql_fix_commits"
+    repo_url = "https://github.com/postgres/postgres.git"
+
+
+class CollectMongodbFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_mongodb_fix_commits"
+    repo_url = "https://github.com/mongodb/mongo.git"
+
+
+class CollectRedisFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_redis_fix_commits"
+    repo_url = "https://github.com/redis/redis.git"
+
+
+class CollectSqliteFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_sqlite_fix_commits"
+    repo_url = "https://github.com/sqlite/sqlite.git"
+
+
+class CollectPhpFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_php_fix_commits"
+    repo_url = "https://github.com/php/php-src.git"
+
+
+class CollectPythonCpythonFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_python_cpython_fix_commits"
+    repo_url = "https://github.com/python/cpython.git"
+
+
+class CollectRubyFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_ruby_fix_commits"
+    repo_url = "https://github.com/ruby/ruby.git"
+
+
+class CollectGoFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_go_fix_commits"
+    repo_url = "https://github.com/golang/go.git"
+
+
+class CollectNodeJsFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_node_js_fix_commits"
+    repo_url = "https://github.com/nodejs/node.git"
+
+
+class CollectRustFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_rust_fix_commits"
+    repo_url = "https://github.com/rust-lang/rust.git"
+
+
+class CollectOpenjdkFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_openjdk_fix_commits"
+    repo_url = "https://github.com/openjdk/jdk.git"
+
+
+class CollectSwiftFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_swift_fix_commits"
+    repo_url = "https://github.com/swiftlang/swift.git"
+
+
+class CollectOpensslFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_openssl_fix_commits"
+    repo_url = "https://github.com/openssl/openssl.git"
+
+
+class CollectDjangoFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_django_fix_commits"
+    repo_url = "https://github.com/django/django.git"
+
+
+class CollectRailsFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_rails_fix_commits"
+    repo_url = "https://github.com/rails/rails.git"
+
+
+class CollectLaravelFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_laravel_fix_commits"
+    repo_url = "https://github.com/laravel/framework.git"
+
+
+class CollectSpringFrameworkFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_spring_framework_fix_commits"
+    repo_url = "https://github.com/spring-projects/spring-framework.git"
+
+
+class CollectReactFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_react_fix_commits"
+    repo_url = "https://github.com/facebook/react.git"
+
+
+class CollectAngularFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_angular_fix_commits"
+    repo_url = "https://github.com/angular/angular.git"
+
+
+class CollectWordpressFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_wordpress_fix_commits"
+    repo_url = "https://github.com/WordPress/WordPress.git"
+
+
+class CollectDockerMobyFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_docker_moby_fix_commits"
+    repo_url = "https://github.com/moby/moby.git"
+
+
+class CollectKubernetesFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_kubernetes_fix_commits"
+    repo_url = "https://github.com/kubernetes/kubernetes.git"
+
+
+class CollectQemuFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_qemu_fix_commits"
+    repo_url = "https://gitlab.com/qemu-project/qemu.git"
+
+
+class CollectXenProjectFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_xen_project_fix_commits"
+    repo_url = "https://github.com/xen-project/xen.git"
+
+
+class CollectVirtualboxFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_virtualbox_fix_commits"
+    repo_url = "https://github.com/mirror/vbox.git"
+
+
+class CollectContainerdFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_containerd_fix_commits"
+    repo_url = "https://github.com/containerd/containerd.git"
+
+
+class CollectAnsibleFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_ansible_fix_commits"
+    repo_url = "https://github.com/ansible/ansible.git"
+
+
+class CollectTerraformFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_terraform_fix_commits"
+    repo_url = "https://github.com/hashicorp/terraform.git"
+
+
+class CollectWiresharkFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_wireshark_fix_commits"
+    repo_url = "https://gitlab.com/wireshark/wireshark.git"
+
+
+class CollectTcpdumpFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_tcpdump_fix_commits"
+    repo_url = "https://github.com/the-tcpdump-group/tcpdump.git"
 
 
 class CollectGitFixCommitsPipeline(CollectVCSFixCommitPipeline):
     pipeline_id = "collect_git_fix_commits"
-    repo_url = "https://github.com/git/git"
+    repo_url = "https://github.com/git/git.git"
 
 
-class CollectTensorflowFixCommitsPipeline(CollectVCSFixCommitPipeline):
-    pipeline_id = "collect_tensorflow_fix_commits"
-    repo_url = "https://github.com/tensorflow/tensorflow"
+class CollectJenkinsFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_jenkins_fix_commits"
+    repo_url = "https://github.com/jenkinsci/jenkins.git"
 
 
-class CollectFirefoxFixCommitsPipeline(CollectVCSFixCommitPipeline):
-    pipeline_id = "collect_firefox_fix_commits"
-    repo_url = "https://github.com/mozilla-firefox/firefox"
-
-
-class CollectQEMUFixCommitsPipeline(CollectVCSFixCommitPipeline):
-    pipeline_id = "collect_qemu_fix_commits"
-    repo_url = "https://github.com/qemu/qemu"
-
-
-class CollectDenoFixCommitsPipeline(CollectVCSFixCommitPipeline):
-    pipeline_id = "collect_deno_fix_commits"
-    repo_url = "https://github.com/denoland/deno"
+class CollectGitlabFixCommitsPipeline(CollectVCSFixCommitPipeline):
+    pipeline_id = "collect_gitlab_fix_commits"
+    repo_url = "https://gitlab.com/gitlab-org/gitlab-foss.git"

22fb93d28b54045c390281aac8c9f5fbe6301f6e
ziad hany <ziadhany2016@gmail.com>
2026-02-10 15:46:36 +0200

    Update retiredotnet to use the new AdvisoryDataV2 Drop duplicates in affected and fixed version range
    Signed-off-by: ziad hany <ziadhany2016@gmail.com>

--- vulnerabilities/pipelines/v2_importers/retiredotnet_importer.py
+++ vulnerabilities/pipelines/v2_importers/retiredotnet_importer.py
@@ -16,7 +16,7 @@ from fetchcode.vcs import fetch_via_vcs
 from packageurl import PackageURL
 from univers.version_range import NugetVersionRange
 
-from vulnerabilities.importer import AdvisoryData
+from vulnerabilities.importer import AdvisoryDataV2
 from vulnerabilities.importer import AffectedPackageV2
 from vulnerabilities.importer import ReferenceV2
 from vulnerabilities.pipelines import VulnerableCodeBaseImporterPipelineV2
@@ -66,7 +66,7 @@ class RetireDotnetImporterPipeline(VulnerableCodeBaseImporterPipelineV2):
                 # group by package name `id`
                 # { pkg_id: {'affected_versions': [], 'fixed': []} }
                 grouped_packages = defaultdict(
-                    lambda: {"affected_versions": [], "fixed_versions": []}
+                    lambda: {"affected_versions": set(), "fixed_versions": set()}
                 )
                 for pkg in json_doc.get("packages") or []:
                     name = pkg.get("id")
@@ -75,11 +75,11 @@ class RetireDotnetImporterPipeline(VulnerableCodeBaseImporterPipelineV2):
 
                     affected_version = pkg.get("affected")
                     if affected_version:
-                        grouped_packages[name]["affected_versions"].append(affected_version)
+                        grouped_packages[name]["affected_versions"].add(affected_version)
 
                     fixed_version = pkg.get("fix")
                     if fixed_version:
-                        grouped_packages[name]["fixed_versions"].append(fixed_version)
+                        grouped_packages[name]["fixed_versions"].add(fixed_version)
 
                 for pkg in grouped_packages:
                     affected_version_range = None
@@ -109,12 +109,12 @@ class RetireDotnetImporterPipeline(VulnerableCodeBaseImporterPipelineV2):
                         )
                     ]
 
-                yield AdvisoryData(
+                yield AdvisoryDataV2(
                     advisory_id=advisory_id,
                     aliases=[aliases] if aliases else [],
                     summary=description,
                     affected_packages=affected_packages,
-                    references_v2=vuln_reference,
+                    references=vuln_reference,
                     url=advisory_url,
                 )
 

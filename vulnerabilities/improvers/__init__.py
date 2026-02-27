209a9242479dc0377881653efe9b05ff0898d0a4
ziad hany <ziadhany2016@gmail.com>
2026-02-27 16:12:17 +0200

    this pipeline should be registered as improver
    Signed-off-by: ziad hany <ziadhany2016@gmail.com>

--- vulnerabilities/improvers/__init__.py
+++ vulnerabilities/improvers/__init__.py
@@ -31,6 +31,7 @@ from vulnerabilities.pipelines.v2_improvers import (
     enhance_with_metasploit as enhance_with_metasploit_v2,
 )
 from vulnerabilities.pipelines.v2_improvers import flag_ghost_packages as flag_ghost_packages_v2
+from vulnerabilities.pipelines.v2_improvers import reference_collect_commits
 from vulnerabilities.pipelines.v2_improvers import relate_severities
 from vulnerabilities.pipelines.v2_improvers import unfurl_version_range as unfurl_version_range_v2
 from vulnerabilities.utils import create_registry
@@ -74,5 +75,6 @@ IMPROVERS_REGISTRY = create_registry(
         compute_advisory_todo.ComputeToDo,
         collect_ssvc_trees.CollectSSVCPipeline,
         relate_severities.RelateSeveritiesPipeline,
+        reference_collect_commits.CollectReferencesFixCommitsPipeline,
     ]
 )

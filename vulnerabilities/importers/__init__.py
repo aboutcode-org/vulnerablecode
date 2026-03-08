aa58447e42eb14924cb37b110444220714440874
newklei <magmacicada@proton.me>
2026-03-08 15:40:48 -0400

    style: sort imports in importers __init__.py
    Signed-off-by: newklei <magmacicada@proton.me>

--- vulnerabilities/importers/__init__.py
+++ vulnerabilities/importers/__init__.py
@@ -56,9 +56,9 @@ from vulnerabilities.pipelines.v2_importers import (
 from vulnerabilities.pipelines.v2_importers import epss_importer_v2
 from vulnerabilities.pipelines.v2_importers import fireeye_importer_v2
 from vulnerabilities.pipelines.v2_importers import gentoo_importer as gentoo_importer_v2
-from vulnerabilities.pipelines.v2_importers import grafana_importer as grafana_importer_v2
 from vulnerabilities.pipelines.v2_importers import github_osv_importer as github_osv_importer_v2
 from vulnerabilities.pipelines.v2_importers import gitlab_importer as gitlab_importer_v2
+from vulnerabilities.pipelines.v2_importers import grafana_importer as grafana_importer_v2
 from vulnerabilities.pipelines.v2_importers import istio_importer as istio_importer_v2
 from vulnerabilities.pipelines.v2_importers import mattermost_importer as mattermost_importer_v2
 from vulnerabilities.pipelines.v2_importers import mozilla_importer as mozilla_importer_v2

55e2c2e3915353752706532662206d60b6cd766d
Tushar Goel <tushar.goel.dav@gmail.com>
2026-03-11 14:51:39 +0530

    Fix formatting issues
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>

--- vulnerabilities/improvers/__init__.py
+++ vulnerabilities/improvers/__init__.py
@@ -20,6 +20,7 @@ from vulnerabilities.pipelines import flag_ghost_packages
 from vulnerabilities.pipelines import populate_vulnerability_summary_pipeline
 from vulnerabilities.pipelines import remove_duplicate_advisories
 from vulnerabilities.pipelines.v2_improvers import collect_ssvc_trees
+from vulnerabilities.pipelines.v2_improvers import compute_advisory_content_hash
 from vulnerabilities.pipelines.v2_improvers import compute_advisory_todo as compute_advisory_todo_v2
 from vulnerabilities.pipelines.v2_improvers import compute_package_risk as compute_package_risk_v2
 from vulnerabilities.pipelines.v2_improvers import (
@@ -33,7 +34,6 @@ from vulnerabilities.pipelines.v2_improvers import (
 from vulnerabilities.pipelines.v2_improvers import flag_ghost_packages as flag_ghost_packages_v2
 from vulnerabilities.pipelines.v2_improvers import relate_severities
 from vulnerabilities.pipelines.v2_improvers import unfurl_version_range as unfurl_version_range_v2
-from vulnerabilities.pipelines.v2_improvers import compute_advisory_content_hash
 from vulnerabilities.utils import create_registry
 
 IMPROVERS_REGISTRY = create_registry(

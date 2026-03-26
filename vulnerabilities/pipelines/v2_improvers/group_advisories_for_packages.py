f562fd85c8440aab8229fcec73774f83c82deb09
Tushar Goel <tushar.goel.dav@gmail.com>
2026-03-27 01:48:13 +0530

    Test out small use case
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>

--- vulnerabilities/pipelines/v2_improvers/group_advisories_for_packages.py
+++ vulnerabilities/pipelines/v2_improvers/group_advisories_for_packages.py
@@ -118,7 +118,7 @@ def get_merged_identifier_groups(advisories):
 
 
 def group_advisoris_for_packages(logger=None):
-    for package in PackageV2.objects.iterator():
+    for package in PackageV2.objects.filter(package_url="pkg:pypi/django@1.5.2").iterator():
         affecting_advisories = AdvisoryV2.objects.latest_affecting_advisories_for_purl(
             purl=package.purl
         ).prefetch_related("aliases")
